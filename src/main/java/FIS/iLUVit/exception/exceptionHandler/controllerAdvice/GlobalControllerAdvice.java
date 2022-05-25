09a520263c663b109baf44ca692d113c0f39184a
digda-5624 <hsg0208@naver.com>
2022-05-25 15:33:25 +0900

    [feat] RuntimeException에 대한 advice 추가

---
+++ src/main/java/FIS/iLUVit/exception/exceptionHandler/controllerAdvice/GlobalControllerAdvice.java
@@ -0,0 +1,29 @@
+package FIS.iLUVit.exception.exceptionHandler.controllerAdvice;
+
+import FIS.iLUVit.exception.exceptionHandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(RuntimeException.class)
+    public ErrorResult RuntimeException(RuntimeException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+    public ErrorResult illegalExHandler(InvalidDataAccessApiUsageException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", "로그인을 해주세요");
+    }
+
+}
