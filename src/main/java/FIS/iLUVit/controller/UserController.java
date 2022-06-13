465db24fbf3bbb1400710684b7eb0e071721444e
Bum <sbl133@naver.com>
2022-06-13 15:37:44 +0900

    [feat] UserController import정리

--- src/main/java/FIS/iLUVit/controller/UserController.java
+++ src/main/java/FIS/iLUVit/controller/UserController.java
@@ -1,19 +1,13 @@
 package FIS.iLUVit.controller;
 
 import FIS.iLUVit.config.argumentResolver.Login;
-import FIS.iLUVit.controller.dto.AuthenticateAuthNumRequest;
 import FIS.iLUVit.controller.dto.UpdatePasswordRequest;
-import FIS.iLUVit.domain.enumtype.AuthKind;
 import FIS.iLUVit.filter.LoginResponse;
-import FIS.iLUVit.service.SignService;
 import FIS.iLUVit.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RestController
