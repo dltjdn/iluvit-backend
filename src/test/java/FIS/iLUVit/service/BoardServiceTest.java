6c63d4104fc302a7684980991c0b75e9c4d26b8b
cy <cy@cyui-MacBookAir.local>
2022-06-27 17:36:30 +0900

    Junit5 의존성 추가 / Board Service 테스트 코드 작성

--- src/test/java/FIS/iLUVit/service/BoardServiceTest.java
+++ src/test/java/FIS/iLUVit/service/BoardServiceTest.java
@@ -2,9 +2,10 @@ package FIS.iLUVit.service;
 
 import FIS.iLUVit.controller.dto.CreateBoardRequest;
 import FIS.iLUVit.domain.Board;
+import FIS.iLUVit.domain.Center;
 import FIS.iLUVit.domain.enumtype.BoardKind;
+import FIS.iLUVit.exception.BoardException;
 import FIS.iLUVit.repository.BoardRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,12 +13,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,25 +32,91 @@ class BoardServiceTest {
     @Test
     public void 모두의_이야기_게시판_추가() throws Exception {
         //given
-        CreateBoardRequest request1 = new CreateBoardRequest("에프아이솔루션", BoardKind.NORMAL);
+        CreateBoardRequest request = new CreateBoardRequest("자유게시판", BoardKind.NORMAL);
+        Board board = Board.createBoard(request.getBoard_name(), request.getBoardKind(), null, false);
+        Long fakeId = 1L;
+        ReflectionTestUtils.setField(board, "id", fakeId);
 
-        Board board1 = Board.createBoard(request1.getBoard_name(), request1.getBoardKind(), null, false);
-
-        Long fakeBoardId = 1L;
-        ReflectionTestUtils.setField(board1, "id", fakeBoardId);
         //mocking
         given(boardRepository.save(any()))
-                .willReturn(board1);
-        given(boardRepository.findById(fakeBoardId))
-                .willReturn(Optional.of(board1));
+                .willReturn(board);
+        given(boardRepository.findById(fakeId))
+                .willReturn(Optional.of(board));
 
         //when
-        Long newBoardId = boardService.create(null, request1);
+        Long newId = boardService.create(null, request);
 
         //then
-        Board findBoard = boardRepository.findById(newBoardId).get();
-        Assertions.assertThat(board1).isEqualTo(findBoard);
+        Board findBoard = boardRepository.findById(newId).get();
+        assertThat(findBoard.getName()).isEqualTo("자유게시판");
+        assertThat(findBoard.getBoardKind()).isEqualTo(BoardKind.NORMAL);
+        assertThat(findBoard.getCenter()).isEqualTo(null);
+        assertThat(findBoard.getIsDefault()).isEqualTo(false);
 
+    }
+
+    @Test
+    public void 모두의_이야기_게시판_추가_중복이름검증() throws Exception {
+        //given
+        CreateBoardRequest request = new CreateBoardRequest("자유게시판", BoardKind.NORMAL);
+        Board board = Board.createBoard(request.getBoard_name(), request.getBoardKind(), null, false);
+
+        //mocking
+        given(boardRepository.findByName(request.getBoard_name()))
+                .willReturn(Optional.of(board));
+
+        //when
+        //the
+        assertThatThrownBy(() -> {
+            boardService.create(null, request);
+        }).isInstanceOf(BoardException.class)
+                .hasMessageContaining("이름 중복");
+    }
+
+    @Test
+    public void 센터의_이야기_게시판_추가() throws Exception {
+        //given
+        CreateBoardRequest request = new CreateBoardRequest("자유게시판", BoardKind.NORMAL);
+        Center center = new Center();
+        Board board = Board.createBoard(request.getBoard_name(), request.getBoardKind(), center, false);
+        Long fakeId = 1L;
+        ReflectionTestUtils.setField(board, "id", fakeId);
+
+        //mocking
+        given(boardRepository.save(any()))
+                .willReturn(board);
+        given(boardRepository.findById(fakeId))
+                .willReturn(Optional.of(board));
+
+        //when
+        Long newId = boardService.create(null, request);
+
+        //then
+        Board findBoard = boardRepository.findById(newId).get();
+        assertThat(findBoard.getName()).isEqualTo("자유게시판");
+        assertThat(findBoard.getBoardKind()).isEqualTo(BoardKind.NORMAL);
+        assertThat(findBoard.getCenter()).isEqualTo(center);
+        assertThat(findBoard.getIsDefault()).isEqualTo(false);
+
+    }
+
+    @Test
+    public void 센터의_이야기_게시판_추가_중복이름검증() throws Exception {
+        //given
+        CreateBoardRequest request = new CreateBoardRequest("자유게시판", BoardKind.NORMAL);
+        Center center = new Center();
+        Board board = Board.createBoard(request.getBoard_name(), request.getBoardKind(), center, false);
+
+        //mocking
+        given(boardRepository.findByName(request.getBoard_name()))
+                .willReturn(Optional.of(board));
+
+        //when
+        //the
+        assertThatThrownBy(() -> {
+            boardService.create(null, request);
+        }).isInstanceOf(BoardException.class)
+                .hasMessageContaining("이름 중복");
     }
 
 }
