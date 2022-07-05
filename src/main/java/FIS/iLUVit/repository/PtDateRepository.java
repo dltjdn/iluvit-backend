408eab4d3c472e04d8bae723588cd4add8d4fd03
digda-5624 <hsg0208@naver.com>
2022-07-05 13:15:18 +0900

    [fix] 설명회 신청 인원 조회 중 쿼리 최적화 및 joined 사용 추가

--- src/main/java/FIS/iLUVit/repository/PtDateRepository.java
+++ src/main/java/FIS/iLUVit/repository/PtDateRepository.java
@@ -19,11 +19,27 @@ public interface PtDateRepository extends JpaRepository<PtDate, Long> {
     Optional<PtDate> findByIdAndJoinParticipation(@Param("id") Long ptDateId);
 
     @Query("select distinct ptDate from PtDate ptDate " +
+            "left join fetch ptDate.participations as participation " +
+            "left join fetch ptDate.presentation as presentation " +
+            "left join fetch presentation.center " +
+            "left join fetch participation.parent " +
+            "where ptDate.id = :id and participation.status = FIS.iLUVit.domain.enumtype.Status.JOINED")
+    Optional<PtDate> findByIdAndJoinParticipationForSearch(@Param("id") Long ptDateId);
+
+    @Query("select distinct ptDate from PtDate ptDate " +
             "left join fetch ptDate.waitings as waiting " +
             "left join fetch ptDate.presentation as presentation " +
             "left join fetch waiting.parent " +
             "where ptDate.id = :ptDateId")
     Optional<PtDate> findByIdAndJoinWaiting(@Param("ptDateId") Long ptDateId);
+
+    @Query("select distinct ptDate from PtDate ptDate " +
+            "left join fetch ptDate.waitings as waiting " +
+            "left join fetch ptDate.presentation as presentation " +
+            "left join fetch presentation.center " +
+            "left join fetch waiting.parent " +
+            "where ptDate.id = :ptDateId")
+    Optional<PtDate> findByIdAndJoinWaitingForSearch(@Param("ptDateId") Long ptDateId);
 
     @Modifying
     @Query("delete from PtDate ptdate where ptdate.id in :ptDateKeys")
