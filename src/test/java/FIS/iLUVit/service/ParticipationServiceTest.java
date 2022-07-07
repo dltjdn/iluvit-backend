5d2ce3f69360e476284ee1825173b0111ce9ad8c
digda-5624 <hsg0208@naver.com>
2022-07-07 11:08:21 +0900

    [fix] empty_composites 추가하여 수정

--- src/test/java/FIS/iLUVit/service/ParticipationServiceTest.java
+++ src/test/java/FIS/iLUVit/service/ParticipationServiceTest.java
@@ -1,6 +1,11 @@
 package FIS.iLUVit.service;
 
+import FIS.iLUVit.repository.ParticipationRepository;
+import FIS.iLUVit.repository.PtDateRepository;
+import FIS.iLUVit.repository.UserRepository;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 class ParticipationServiceTest {
 
@@ -12,9 +17,23 @@ class ParticipationServiceTest {
      * 3. 설명회를 이미 신청했을 경우
      */
 
+    @Mock
+    UserRepository userRepository;
+    @Mock
+    ParticipationRepository participationRepository;
+    @Mock
+    PtDateRepository ptDateRepository;
+    @InjectMocks
+    ParticipationService participationService;
+
+
     @Test
     public void 설명회_신청_신청기간이지남_예외() throws Exception {
         //given
+        /**
+         * 설명회 신청을 위해서는 설명회 정보가 필요하다.
+         * 컨트롤러에서 넘어온
+         */
 
         //when
 
