f90d115180f86006baecfffebaad3876921d79ca
digda-5624 <hsg0208@naver.com>
2022-07-11 13:44:28 +0900

    [fix] 설명회 검색 날짜로 찾기 검색값 수정

--- src/main/java/FIS/iLUVit/domain/ChildHouse.java
+++ src/main/java/FIS/iLUVit/domain/ChildHouse.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class ChildHouse extends Center{
 
-    @Builder
+    @Builder(builderMethodName = "cBuilder")
     public ChildHouse(Long id, String name, String owner, String director, String estType, String status, String estDate, String tel, String homepage, String startTime, String endTime, Integer minAge, Integer maxAge, String address, String zipcode, Area area, Double longitude, Double latitude, String offerService, Integer maxChildCnt, Integer curChildCnt, LocalDate updateDate, Boolean signed, Boolean recruit, Integer waitingNum, String introText, Integer imgCnt, Integer videoCnt, Integer score, String addInfo, String program, KindOf kindOf, ClassInfo classInfo, TeacherInfo teacherInfo, CostInfo costInfo, BasicInfra basicInfra, Theme theme, OtherInfo otherInfo) {
         super(id, name, owner, director, estType, status, estDate, tel, homepage, startTime, endTime, minAge, maxAge, address, zipcode, area, longitude, latitude, offerService, maxChildCnt, curChildCnt, updateDate, signed, recruit, waitingNum, introText, imgCnt, videoCnt, score, addInfo, program, kindOf, classInfo, teacherInfo, costInfo, basicInfra, theme, otherInfo);
     }
