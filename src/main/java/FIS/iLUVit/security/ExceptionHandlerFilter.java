59c7786ba7da5326a03900bf5e75463961c738a7
Bum <sbl133@naver.com>
2022-08-01 16:05:00 +0900

    [feat] ExceptionHandlerFilter 추가

---
+++ src/main/java/FIS/iLUVit/security/ExceptionHandlerFilter.java
@@ -0,0 +1,37 @@
+package FIS.iLUVit.security;
+
+import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (JWTVerificationException e) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNAUTHORIZED, e.getMessage());
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(convertObjectToJson(errorResponse));
+        }
+    }
+
+    public String convertObjectToJson(Object object) throws JsonProcessingException {
+        if (object == null) {
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
+    }
+}
--- src/test/java/FIS/iLUVit/integration/UserControllerTest.java
+++ src/test/java/FIS/iLUVit/integration/UserControllerTest.java
@@ -1,5 +1,7 @@
 package FIS.iLUVit.integration;
 
+import FIS.iLUVit.controller.dto.TokenRefreshRequest;
+import FIS.iLUVit.domain.Parent;
 import FIS.iLUVit.domain.TokenPair;
 import FIS.iLUVit.domain.User;
 import FIS.iLUVit.repository.TokenPairRepository;
@@ -9,22 +11,25 @@ import FIS.iLUVit.security.LoginRequest;
 import FIS.iLUVit.security.LoginResponse;
 import FIS.iLUVit.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserControllerTest {
@@ -33,15 +38,34 @@ public class UserControllerTest {
     private TokenPairRepository tokenPairRepository;
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private UserService userService;
-    @Spy
+    @SpyBean
     private JwtUtils jwtUtils;
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
     @Autowired
     private MockMvc mockMvc;
 
     private ObjectMapper objectMapper = new ObjectMapper();
+
+    private String loginId;
+    private String password;
+    private Parent parent;
+
+    @BeforeEach
+    public void init() {
+        objectMapper = new ObjectMapper();
+
+        loginId = "loginId";
+        password = "password";
+        parent = Parent.builder()
+                .loginId(loginId)
+                .password(encoder.encode(password))
+                .build();
+    }
     @Nested
     @DisplayName("로그인")
     class login{
@@ -49,8 +73,9 @@ public class UserControllerTest {
         @DisplayName("[error] 로그인아이디 틀림")
         public void 로그인아이디틀림() throws Exception {
             // given
+            userRepository.save(parent);
             String url = "/login";
-            LoginRequest request = new LoginRequest("!@#!QAZ", "asd");
+            LoginRequest request = new LoginRequest("!@#!QAZ", password);
             // when
             ResultActions result = mockMvc.perform(
                     MockMvcRequestBuilders.post(url)
@@ -65,8 +90,9 @@ public class UserControllerTest {
         @DisplayName("[error] 비밀번호 틀림")
         public void 비밀번호틀림() throws Exception {
             // given
+            userRepository.save(parent);
             String url = "/login";
-            LoginRequest request = new LoginRequest("asd", "!@#QAZ");
+            LoginRequest request = new LoginRequest(loginId, "!@#QAZ");
             // when
             ResultActions result = mockMvc.perform(
                     MockMvcRequestBuilders.post(url)
@@ -81,11 +107,9 @@ public class UserControllerTest {
         @DisplayName("[success] 로그인성공")
         public void 로그인성공() throws Exception {
             // given
+            userRepository.save(parent);
             String url = "/login";
-            String loginId = "asd";
-            String pwd = "asd";
-            LoginRequest request = new LoginRequest(loginId, pwd);
-            Mockito.doReturn()
+            LoginRequest request = new LoginRequest(loginId, password);
             // when
             ResultActions result = mockMvc.perform(
                     MockMvcRequestBuilders.post(url)
@@ -105,4 +129,26 @@ public class UserControllerTest {
                     ));
         }
     }
+
+//    @Nested
+//    @DisplayName("refreshToken를 이용한 토큰 재발급")
+//    class refresh{
+//        @Test
+//        @DisplayName("[error] refreshToken 유효성검사 실패")
+//        public void 유효성검사실패() {
+//            // given
+//            userRepository.save(parent);
+//            userService.login(new LoginRequest(loginId, password));
+//            Mockito.doReturn()
+//            User user = userRepository.findByLoginId(loginId).get();
+//            String url = "/refresh";
+//            TokenRefreshRequest request = new TokenRefreshRequest();
+//            // when
+//
+//            // then
+//
+//        }
+//    }
+
+
 }
