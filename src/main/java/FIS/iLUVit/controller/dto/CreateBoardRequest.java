df2b993e577c72cd021b07df8fc94a3cbc390d73
cy <cy@cyui-MacBookAir.local>
2022-08-08 17:06:39 +0900

    [fix] 게시판 생성 request DTO 유효성 검사

--- src/main/java/FIS/iLUVit/controller/dto/CreateBoardRequest.java
+++ src/main/java/FIS/iLUVit/controller/dto/CreateBoardRequest.java
@@ -5,10 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateBoardRequest {
+    @NotBlank(message = "게시판 이름을 채워주세요.")
     private String board_name;
+    @NotBlank(message = "게시판 종류를 설정해주세요.")
     private BoardKind boardKind;
 }
