5bfcabae048aff5ac6807c799729de7d5aefaef2
digda-5624 <hsg0208@naver.com>
2022-08-11 11:10:44 +0900

    [fix] 알림 정보 dto 수정

--- src/main/java/FIS/iLUVit/domain/alarms/CenterApprovalAcceptedAlarm.java
+++ src/main/java/FIS/iLUVit/domain/alarms/CenterApprovalAcceptedAlarm.java
@@ -17,27 +17,31 @@ public class CenterApprovalAcceptedAlarm extends Alarm{
 
     @Column(name = "centerId")
     private Long centerId;
+    private String centerName;
 
     public CenterApprovalAcceptedAlarm(User user, Center center) {
         super(user);
         this.mode = AlarmUtils.CENTER_APPROVAL_ACCEPTED;
         this.centerId = center.getId();
+        this.centerName = center.getName();
         String args[] = {center.getName()};
         message = AlarmUtils.getMessage(mode, args);
     }
 
     @Override
     public AlarmDto exportAlarm() {
-        return new CenterApprovalAcceptedAlarmDto(id, createdDate, message, dtype, centerId);
+        return new CenterApprovalAcceptedAlarmDto(id, createdDate, message, dtype, centerId, centerName);
     }
 
     @Getter
     public static class CenterApprovalAcceptedAlarmDto extends AlarmDto{
         protected Long centerId;
+        protected String centerName;
 
-        public CenterApprovalAcceptedAlarmDto(Long id, LocalDateTime createdDate, String message, String type, Long centerId) {
+        public CenterApprovalAcceptedAlarmDto(Long id, LocalDateTime createdDate, String message, String type, Long centerId, String centerName) {
             super(id, createdDate, message, type);
             this.centerId = centerId;
+            this.centerName = centerName;
         }
     }
 }
