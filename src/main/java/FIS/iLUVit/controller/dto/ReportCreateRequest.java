f5e2dacfe7532458709c440936b2e0bfa2e6b0f2
minah9999 <81949526+minah9999@users.noreply.github.com>
2022-09-21 15:07:09 +0900

    [update] 신고기능 dto 수정

--- src/main/java/FIS/iLUVit/controller/dto/ReportCreateRequest.java
+++ src/main/java/FIS/iLUVit/controller/dto/ReportCreateRequest.java
@@ -7,7 +7,6 @@ import lombok.Data;
 @Data
 public class ReportCreateRequest {
     private Long targetId;          // 신고할 대상의 id
-    private Long targetUserId;      // 신고할 대상의 작성자 id
     private ReportType type;        // 신고할 대상의 타입
     private ReportReason reason;    // 신고 사유
 }
