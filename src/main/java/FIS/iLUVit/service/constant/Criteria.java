dd0b1e6d4433352c47606b1ff21bd33f69c051ee
rooni97 <rooni97@naver.com>
2022-10-25 15:47:57 +0900

    [feat] 전역변수처럼 사용 abstract

--- src/main/java/FIS/iLUVit/service/constant/Criteria.java
+++ src/main/java/FIS/iLUVit/service/constant/Criteria.java
@@ -1,6 +1,6 @@
 package FIS.iLUVit.service.constant;
 
-public class Criteria {
+public abstract class Criteria {
 
     public static final Integer HOT_POST_HEART_CNT = 10; // 핫 게시판에 등록될 게시글 좋아요 개수 기준
 }
