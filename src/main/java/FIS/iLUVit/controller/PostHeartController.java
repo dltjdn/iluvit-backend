3033da6081efa7d74b66c08c77e65b7a3015f833
박찬희 <chani@bagchanhuiui-MacBookAir.local>
2023-01-12 16:42:44 +0900

    [CHORE] PostHeartController 수정

--- src/main/java/FIS/iLUVit/controller/PostHeartController.java
+++ src/main/java/FIS/iLUVit/controller/PostHeartController.java
@@ -11,13 +11,11 @@ import FIS.iLUVit.repository.PostRepository;
 import FIS.iLUVit.repository.UserRepository;
 import FIS.iLUVit.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("postHeart")
 public class PostHeartController {
 
     private final PostHeartRepository postHeartRepository;
@@ -28,7 +26,7 @@ public class PostHeartController {
         작성시간: 2022/06/27 1:35 PM
         내용: 게시글 좋아요
     */
-    @PostMapping("/user/postHeart/post/{post_id}")
+    @PostMapping("user/post/{post_id}")
     public Long like(@Login Long userId, @PathVariable("post_id") Long postId) {
         return postService.savePostHeart(userId, postId);
     }
@@ -38,10 +36,11 @@ public class PostHeartController {
         작성시간: 2022/06/27 1:39 PM
         내용: 게시글 좋아요 취소, 기존에 좋아요 눌렀던 상태여야 취소 가능
     */
-    @DeleteMapping("/user/postHeart/post/{post_id}")
+    @DeleteMapping("user/post/{post_id}")
     public void cancel(@Login Long userId, @PathVariable("post_id") Long postId) {
         PostHeart postHeart = postHeartRepository.findByPostAndUser(userId, postId)
                 .orElseThrow(() -> new PostException(PostErrorResult.POST_NOT_EXIST));
         postHeartRepository.delete(postHeart);
     }
 }
+ㄴ
