4e4522ea99691ccb35ded3f16aa74282941e59af
yeongeun <yeongeun@yeongeunui-MacBookAir.local>
2023-01-12 16:10:28 +0900

    [CHORE] AuthNumberController url 수정

--- src/main/java/FIS/iLUVit/controller/AuthNumberController.java
+++ src/main/java/FIS/iLUVit/controller/AuthNumberController.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/authNumber")
+@RequestMapping("authNumber")
 public class AuthNumberController {
 
     private final AuthNumberService authNumberService;
@@ -22,7 +22,7 @@ public class AuthNumberController {
      * 작성자: 이승범
      * 작성내용: 회원가입 위한 인증번호 전송
      */
-    @GetMapping("/signup")
+    @GetMapping("signup")
     public void sendAuthNumberForSignup(@RequestParam String phoneNumber) {
         authNumberService.sendAuthNumberForSignup(phoneNumber);
     }
@@ -32,7 +32,7 @@ public class AuthNumberController {
      * 작성자: 이승범
      * 작성내용: 로그인 아이디를 찾기위한 인증번호 전송
      */
-    @GetMapping("/loginId")
+    @GetMapping("loginId")
     public void sendAuthNumberForFindLoginId(@RequestParam String phoneNumber) {
         authNumberService.sendAuthNumberForFindLoginId(phoneNumber);
     }
@@ -42,7 +42,7 @@ public class AuthNumberController {
      * 작성자: 이승범
      * 작성내용: 비밀번호 찾기 인증번호 전송
      */
-    @GetMapping("/password")
+    @GetMapping("password")
     public void sendAuthNumberForFindPassword(@RequestParam String loginId, @RequestParam String phoneNumber) {
         authNumberService.sendAuthNumberForFindPassword(loginId, phoneNumber);
     }
@@ -52,7 +52,7 @@ public class AuthNumberController {
     *   작성자: 이승범
     *   작성내용: 핸드폰번호 변경을 위한 인증번호 전송
     */
-    @GetMapping("/user/phoneNumber")
+    @GetMapping("user/phoneNumber")
     public void sendAuthNumberForUpdatePhoneNum(@Login Long userId, @RequestParam String phoneNumber) {
         authNumberService.sendAuthNumberForChangePhone(userId, phoneNumber);
     }
@@ -62,7 +62,7 @@ public class AuthNumberController {
      * 작성자: 이승범
      * 작성내용: 인증번호를 통한 핸드폰 인증 (회원가입, 비밀번호 찾기, 핸드폰번호 변경)
      */
-    @PostMapping
+    @PostMapping("")
     public void authenticateAuthNum(@Login Long userId, @RequestBody AuthenticateAuthNumRequest request) {
         authNumberService.authenticateAuthNum(userId, request);
     }
@@ -72,7 +72,7 @@ public class AuthNumberController {
      * 작성자: 이승범
      * 작성내용: 인증번호를 통한 로그인 아이디 찾기
      */
-    @PostMapping("/findLoginId")
+    @PostMapping("findLoginId")
     public String findLoginId(@RequestBody AuthenticateAuthNumRequest request) {
         return authNumberService.findLoginId(request);
     }
@@ -82,7 +82,7 @@ public class AuthNumberController {
     *   작성자: 이승범
     *   작성내용: 비밀번호 찾기 근데 이제 변경을 곁들인
     */
-    @PostMapping("/findPassword")
+    @PostMapping("findPassword")
     public void findPassword(@RequestBody @Valid FindPasswordRequest request) {
         authNumberService.changePassword(request);
     }
