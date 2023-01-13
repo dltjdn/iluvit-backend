68bb64b39fad95614540fbe24fdc531348252a66
yeongeun <yeongeun@yeongeunui-MacBookAir.local>
2023-01-13 11:50:03 +0900

    [CHORE] ReviewController url 수정

--- src/main/java/FIS/iLUVit/controller/ReviewController.java
+++ src/main/java/FIS/iLUVit/controller/ReviewController.java
@@ -19,7 +19,7 @@ public class ReviewController {
         작성시간: 2022/06/27 1:42 PM
         내용: 학부모가 작성한 리뷰 조회
     */
-    @GetMapping("parent")
+    @GetMapping("")
     public ReviewByParentDTO searchByParent(@Login Long userId, Pageable pageable) { // @Login 어노테이션 달아야됨.
         return reviewService.findByParent(userId, pageable);
     }
@@ -29,7 +29,7 @@ public class ReviewController {
         작성시간: 2022/06/27 1:49 PM
         내용: 학부모가 쓴 리뷰 등록
     */
-    @PostMapping("parent")
+    @PostMapping("")
     public Long registerReview(@Login Long userId, @RequestBody ReviewCreateDTO reviewCreateDTO) {
         return reviewService.saveReview(userId, reviewCreateDTO);
     }
@@ -39,8 +39,8 @@ public class ReviewController {
         작성시간: 2022/06/27 1:49 PM
         내용: 리뷰 수정
     */
-    @PatchMapping("parent/{review_id}")
-    public void updateReview(@Login Long userId, @PathVariable(name = "review_id") Long reviewId,
+    @PatchMapping("{reviewId}")
+    public void updateReview(@Login Long userId, @PathVariable(name = "reviewId") Long reviewId,
                              @RequestBody ReviewUpdateDTO reviewUpdateDto) {
         reviewService.updateReview(reviewId, userId, reviewUpdateDto.getContent());
     }
@@ -50,8 +50,8 @@ public class ReviewController {
         작성시간: 2022/06/27 1:49 PM
         내용: 리뷰 삭제
     */
-    @DeleteMapping("parent/{review_id}")
-    public void deleteReview(@Login Long userId, @PathVariable("review_id") Long reviewId) {
+    @DeleteMapping("{reviewId}")
+    public void deleteReview(@Login Long userId, @PathVariable("reviewId") Long reviewId) {
         reviewService.deleteReview(reviewId, userId);
     }
 
@@ -60,8 +60,8 @@ public class ReviewController {
         작성시간: 2022/06/27 1:49 PM
         내용: 센터에 올라온 리뷰들 조회
     */
-    @GetMapping("center/{center_id}")
-    public ReviewByCenterDTO searchByCenter(@PathVariable(name = "center_id") Long centerId,
+    @GetMapping("center/{centerId}")
+    public ReviewByCenterDTO searchByCenter(@PathVariable(name = "centerId") Long centerId,
                                             Pageable pageable) {
         return reviewService.findByCenter(centerId, pageable);
     }
@@ -71,8 +71,8 @@ public class ReviewController {
         작성시간: 2022/06/27 1:50 PM
         내용: 시설에 달린 리뷰 답글 등록 + 수정, 답글 달 수 있는 권한은 Director 만 가능
      */
-    @PostMapping("teacher/{review_id}/comment")
-    public Long registerComment(@Login Long teacherId, @PathVariable("review_id") Long reviewId,
+    @PostMapping("{reviewId}/comment")
+    public Long registerComment(@Login Long teacherId, @PathVariable("reviewId") Long reviewId,
                                 @RequestBody ReviewCommentDTO reviewCommentDTO) {
         return reviewService.saveComment(reviewId, reviewCommentDTO.getComment(), teacherId);
     }
@@ -82,8 +82,8 @@ public class ReviewController {
         작성시간: 2022/06/27 1:52 PM
         내용: 시설에 달린 리뷰 답글 삭제
     */
-    @DeleteMapping("teacher/{review_id}/comment")
-    public void deleteComment(@Login Long teacherId, @PathVariable("review_id") Long reviewId) {
+    @DeleteMapping("{reviewId}/comment")
+    public void deleteComment(@Login Long teacherId, @PathVariable("reviewId") Long reviewId) {
         reviewService.deleteComment(reviewId, teacherId);
     }
 }
