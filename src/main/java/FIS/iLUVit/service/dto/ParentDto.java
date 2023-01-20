a3e33e3b937e882dad4ffa3d980c31cbaab11c8f
yeongeun <yeongeun@yeongeunui-MacBookAir.local>
2023-01-20 15:08:27 +0900

    [CHORE] Service Dto renaming

--- src/main/java/FIS/iLUVit/service/dto/ParentDto.java
+++ src/main/java/FIS/iLUVit/service/dto/ParentDto.java
@@ -3,16 +3,14 @@ package FIS.iLUVit.service.dto;
 import FIS.iLUVit.domain.Parent;
 import lombok.Data;
 
-import javax.persistence.Column;
-
 @Data
-public class ParentInfoForDirectorDto {
+public class ParentDto {
 
     private String phoneNumber;         // 핸드폰 번호
     private String emailAddress;        // 이메일
     private String name;                // 잔짜 이름
 
-    public ParentInfoForDirectorDto(Parent parent) {
+    public ParentDto(Parent parent) {
         phoneNumber = parent.getPhoneNumber();
         emailAddress = parent.getEmailAddress();
         name = parent.getName();
