a05e1138a41a8e91fd508babf06e4931f2ae228e
dltjdn <hana40004@naver.com>
2023-02-10 18:11:03 +0900

    [REFACTOR] 기타 @Data 제거

--- src/main/java/FIS/iLUVit/security/uesrdetails/PrincipalDetails.java
+++ src/main/java/FIS/iLUVit/security/uesrdetails/PrincipalDetails.java
@@ -1,18 +1,15 @@
 package FIS.iLUVit.security.uesrdetails;
 
 import FIS.iLUVit.domain.User;
-import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
-@Data
+@Getter
 @RequiredArgsConstructor
 public class PrincipalDetails implements UserDetails {
 
