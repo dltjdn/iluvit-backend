327b3add580596886816a54478f857f112c9cb85
dltjdn <hana40004@naver.com>
2023-02-15 17:45:22 +0900

    [FIX] comment test code 오류 수정

--- src/main/java/FIS/iLUVit/dto/comment/CommentRequest.java
+++ src/main/java/FIS/iLUVit/dto/comment/CommentRequest.java
@@ -1,9 +1,11 @@
 package FIS.iLUVit.dto.comment;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CommentRequest {
     private String content;
     private Boolean anonymous;
