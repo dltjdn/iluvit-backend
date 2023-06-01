b17f4cbc79445ecc4bf42dc666045a0daec48746
박찬희 <chani@bagchanhuiui-MacBookAir.local>
2023-06-01 16:42:12 +0900

    [ADD] ExpoTokenRepository 주석 작성 완료 #284

--- src/main/java/FIS/iLUVit/repository/ExpoTokenRepository.java
+++ src/main/java/FIS/iLUVit/repository/ExpoTokenRepository.java
@@ -10,15 +10,33 @@ import java.util.Optional;
 
 public interface ExpoTokenRepository extends JpaRepository<ExpoToken, Long> {
 
+    /*
+        사용자로 expo 토큰을 조회합니다.
+     */
     List<ExpoToken> findByUser(User user);
 
+    /*
+        토큰으로 expo 토큰을 조회합니다.
+     */
     Optional<ExpoToken> findByToken(String token);
 
+    /*
+        토큰 및 사용자별로 삭제합니다.
+     */
     void deleteByTokenAndUser(String token, User user);
 
+    /*
+        토큰 및 사용자로 expo 토큰을 조회합니다.
+     */
     Optional<ExpoToken> findByTokenAndUser(String token, User user);
 
+    /*
+        토큰 인으로 삭제합니다.
+     */
     void deleteByTokenIn(Collection<String> tokens);
 
+    /*
+        사용자별로 모두 삭제합니다.
+     */
     void deleteAllByUser(User user);
 }
