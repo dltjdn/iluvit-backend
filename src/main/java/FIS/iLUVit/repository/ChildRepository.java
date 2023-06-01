9fa7207df0fdd0cff10df6e7c4fa03292aa5916c
박찬희 <chani@bagchanhuiui-MacBookAir.local>
2023-06-01 16:32:10 +0900

    [ADD] ChildRepository 주석 작성 완료 #284

--- src/main/java/FIS/iLUVit/repository/ChildRepository.java
+++ src/main/java/FIS/iLUVit/repository/ChildRepository.java
@@ -13,6 +13,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
+
+    /*
+        유저 id와 시설 id를 파라미터로 받아서 부모와 시설로 조회합니다.
+     */
     @Query("select c " +
             "from Child c " +
             "join c.parent p " +
@@ -21,6 +25,9 @@ public interface ChildRepository extends JpaRepository<Child, Long> {
             "and ct.id = :centerId")
     List<Child> findByParentAndCenter(@Param("userId") Long userId, @Param("centerId") Long centerId);
 
+    /*
+        유저 id와 아이 id를 파라미터로 받아서 센터가 있는 ID 및 부모로 조회합니다.
+     */
     @Query("select c " +
             "from Child c " +
             "left join fetch c.center " +
@@ -28,21 +35,32 @@ public interface ChildRepository extends JpaRepository<Child, Long> {
             "and c.parent.id =:userId")
     Optional<Child> findByIdAndParentWithCenter(@Param("userId") Long userId, @Param("childId") Long childId);
 
+    /*
+        유저 id와 아이 id를 파라미터로 받아서 아이디와 부모로 조회합니다.
+     */
     @Query("select c " +
             "from Child c " +
             "where c.id =:childId " +
             "and c.parent.id =:userId")
     Optional<Child> findByIdAndParent(@Param("userId") Long userId, @Param("childId") Long childId);
 
+    /*
+        유저 id를 파라미터로 받아서 센터가 있는 사용자 id로 조회합니다.
+     */
     @Query("select c " +
             "from Child c " +
             "left join fetch c.center " +
             "where c.parent.id =:userId")
     List<Child> findByUserWithCenter(@Param("userId") Long userId);
 
+    /*
+        부모로 조회합니다.
+     */
     List<Child> findByParent(Parent parent);
 
+    /*
+        시설로 조회합니다.
+     */
     List<Child> findByCenter(Center center);
-
 
 }
