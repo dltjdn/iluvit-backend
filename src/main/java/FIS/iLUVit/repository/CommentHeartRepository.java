2031eb1d0414dec314b184806641e092b4412509
박찬희 <chani@bagchanhuiui-MacBookAir.local>
2023-06-01 16:33:40 +0900

    [ADD] CommentHeartRepository 주석 작성 완료

--- src/main/java/FIS/iLUVit/repository/CommentHeartRepository.java
+++ src/main/java/FIS/iLUVit/repository/CommentHeartRepository.java
@@ -8,6 +8,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface CommentHeartRepository extends JpaRepository<CommentHeart, Long> {
+
+    /*
+        유저 id와 댓글 id를 파라미터로 받아서 사용자 및 댓글로 조회합니다.
+     */
     @Query("select ch " +
             "from CommentHeart ch " +
             "join fetch ch.comment c " +
