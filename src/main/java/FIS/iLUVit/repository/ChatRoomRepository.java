35fd247a6712dc1794c14bbe3b8ddc28da4951d5
박찬희 <chani@bagchanhuiui-MacBookAir.local>
2023-06-01 16:21:10 +0900

    [ADD] ChatRoomRepository 주석 작성 완료 #284

--- src/main/java/FIS/iLUVit/repository/ChatRoomRepository.java
+++ src/main/java/FIS/iLUVit/repository/ChatRoomRepository.java
@@ -15,10 +15,19 @@ import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
+    /*
+        수신자 및 발신자 및 우편으로 조회합니다.
+     */
     Optional<ChatRoom> findByReceiverAndSenderAndPost(User receiver, User sender, Post post);
 
+    /*
+        수신자 및 발신자 및 게시물 및 익명으로 조회합니다.
+     */
     Optional<ChatRoom> findByReceiverAndSenderAndPostAndAnonymous(User receiver, User sender, Post post, Boolean anonymous);
 
+    /*
+        (자기자신)유저 id와 (상대방)유저 id를 파라미터로 받아서 유저로 채팅방을 조회합니다.
+     */
     @Query("select cr from ChatRoom cr " +
             "left join fetch cr.post p " +
             "left join fetch p.board b " +
@@ -26,6 +35,9 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
             "where cr.receiver.id = :userId order by cr.updatedDate desc ")
     Slice<ChatRoom> findByUser(@Param("userId") Long userId, @Param("userId") Pageable pageable);
 
+    /*
+        여러 개의 게시글 id를 파라미터로 받아서 게시글 id와 댓글 id를 null로 지정합니다.
+     */
     @Modifying(clearAutomatically = true)
     @Query("update ChatRoom cr set cr.post.id = null, cr.comment.id = null where cr.post.id in :postIds ")
     Integer setPostIsNull(@Param("postIds") Collection<Long> postIds);
