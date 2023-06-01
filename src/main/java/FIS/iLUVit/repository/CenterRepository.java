b81f074ad273e4c0d27842dfc9f87d2bac5dedf6
박찬희 <chani@bagchanhuiui-MacBookAir.local>
2023-06-01 16:10:20 +0900

    [ADD] CenterRepository 주석 추가 완료 #284

--- src/main/java/FIS/iLUVit/repository/CenterRepository.java
+++ src/main/java/FIS/iLUVit/repository/CenterRepository.java
@@ -9,6 +9,9 @@ import java.util.Optional;
 
 public interface CenterRepository extends JpaRepository<Center, Long>, CenterRepositoryCustom {
 
+    /*
+        사용자 id를 파라미터로 받아서 사용자로 조회합니다.
+     */
     @Query("select ct " +
             "from Child c " +
             "join c.center ct" +
@@ -16,8 +19,14 @@ public interface CenterRepository extends JpaRepository<Center, Long>, CenterRep
             "where p.id = :userId")
     List<Center> findByUser(@Param("userId") Long userId);
 
+    /*
+        시설 Id로 조회합니다.
+     */
     Optional<Center> findById(Long centerId);
 
+    /*
+        시설 Id를 파라미터로 받아서 교사의 서명과 id로 조회합니다.
+     */
     @Query("select c " +
             "from Teacher t " +
             "join t.center c " +
