a5d1a6dec02984f9de30d7b8c14ec2d5bc07cd11
박찬희 <chani@bagchanhuiui-MacBookAir.local>
2023-06-08 15:04:44 +0900

    [ADD] WaitingRepository 주석 작성 완료 #284

--- src/main/java/FIS/iLUVit/repository/WaitingRepository.java
+++ src/main/java/FIS/iLUVit/repository/WaitingRepository.java
@@ -16,36 +16,55 @@ import java.util.Optional;
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
 
 //    @Lock(LockModeType.PESSIMISTIC_WRITE)
+
+    /*
+        설명회 날짜를 파라미터로 받아서 최소 대기 주문을 조회합니다.
+     */
     @Query("select waiting from Waiting waiting " +
             "join fetch waiting.parent " +
             "where waiting.ptDate = :ptDate " +
             "and waiting.waitingOrder = (select min(w.waitingOrder) from Waiting w where w.ptDate =:ptDate) ")
     Waiting findMinWaitingOrder(@Param("ptDate") PtDate ptDate);
 
+    /*
+        설명회 날짜와 변경 번호를 파라미터로 받아서 설명회 날짜 및 주문 번호로 대기들을 조회합니다.
+     */
     @Query("select distinct waiting from Waiting waiting " +
             "join fetch waiting.ptDate as ptDate " +
             "join fetch ptDate.participations " +
             "where waiting.ptDate = :ptDate and waiting.waitingOrder <= :changeNum")
     List<Waiting> findWaitingsByPtDateAndOrderNum(@Param("ptDate") PtDate ptDate, @Param("changeNum") Integer changeNum);
 
+    /*
+        변경 번호와 설명회 날짜를 파라미터로 받아서 설명회 날짜 변경을 위한 대기 순서를 업데이트합니다.
+     */
     @Modifying
     @Query("update Waiting waiting " +
             "set waiting.waitingOrder = waiting.waitingOrder - :changeNum " +
             "where waiting.ptDate = :ptDate")
     void updateWaitingOrderForPtDateChange(@Param("changeNum") Integer changeNum, @Param("ptDate") PtDate ptDate);
 
+    /*
+        대기 id와 사용자 id를 파라미터로 받아서 설명회 날짜에 ID로 조회합니다.
+     */
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select waiting from Waiting waiting " +
             "join fetch waiting.ptDate " +
             "where waiting.id = :waitingId and waiting.parent.id = :userId")
     Optional<Waiting> findByIdWithPtDate(@Param("waitingId") Long waitingId, @Param("userId") Long userId);
 
+    /*
+        설명회 날짜와 대기 주문을 파라미터로 받아서 대기 주문을 업데이트합니다.
+     */
     @Modifying
     @Query("update Waiting waiting " +
             "set waiting.waitingOrder = waiting.waitingOrder - 1 " +
             "where waiting.waitingOrder > :waitingOrder and waiting.ptDate = :ptDate ")
     void updateWaitingOrder(@Param("ptDate")PtDate ptDate, @Param("waitingOrder") Integer waitingOrder);
 
+    /*
+        부모로 대기를 조회합니다.
+     */
     List<Waiting> findByParent(Parent parent);
 
 }
