8dfa5af920815a0b00b47b9528636a598403cefd
dltjdn <hana40004@naver.com>
2023-06-20 14:12:05 +0900

    [CHORE] run_new_was.sh 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 PROJECT_ROOT="/home/ubuntu/iluvit/app" # 프로젝트 루트
-JAR_FILE="$PROJECT_ROOT/build/libs/iLUVit-0.0.1-SNAPSHOT.jar" # JAR_FILE (어쩌구저쩌구.jar)
+JAR_FILE="$PROJECT_ROOT/iLUVit-0.0.1-SNAPSHOT.jar"
 
 # service_url.inc 에서 현재 서비스를 하고 있는 WAS의 포트 번호 가져오기
 CURRENT_PORT=$(cat /home/ubuntu/iluvit/service_url.inc | grep -Po '[0-9]+' | tail -1)
@@ -28,7 +28,11 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill -9 ${TARGET_PID}
 fi
 
+# build 파일 복사
+echo "$TIME_NOW > $JAR_FILE 파일 복사"
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+
 # 타켓 포트에 jar파일을 이용해 새로운 서버 실행
-nohup java -jar -Dserver.port=${TARGET_PORT} $PROJECT_ROOT/build/libs/iLUVit-0.0.1-SNAPSHOT.jar > /home/ubuntu/iluvit/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} $JAR_FILE > /home/ubuntu/iluvit/app/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
