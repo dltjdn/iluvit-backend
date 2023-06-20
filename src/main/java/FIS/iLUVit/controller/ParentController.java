d8b240858a11660319ae407fc00cf9607f3de0ba
dltjdn <hana40004@naver.com>
2023-06-20 21:47:15 +0900

    [TEST] prod cd test

--- src/main/java/FIS/iLUVit/controller/ParentController.java
+++ src/main/java/FIS/iLUVit/controller/ParentController.java
@@ -57,7 +57,7 @@ public class ParentController {
      */
     @DeleteMapping("withdraw")
     public Long deleteParent(@Login Long userId){
-        return 3333L;
+        return 444L;
         //return parentService.withdrawParent(userId);
     }
 }
