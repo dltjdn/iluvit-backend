03e91b7ac04d7a45cfd67242475e7c794608300a
chani <qkrcks0426@naver.com>
2023-06-27 16:06:28 +0900

    [UPDATE] ReviewRepository #284

--- src/main/java/FIS/iLUVit/repository/ReviewRepository.java
+++ src/main/java/FIS/iLUVit/repository/ReviewRepository.java
@@ -15,7 +15,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long>{
 
 
     /*
-        학부모가 작성한 리뷰를 조회합니다.
+        부모가 작성한 리뷰를 조회합니다.
      */
     @Query("select r from Review r join fetch r.center c join fetch r.parent p where p.id = :parentId")
     Slice<Review> findByParent(@Param("parentId") Long parentId, Pageable pageable);
