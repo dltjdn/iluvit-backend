11c03a138f1d72a63d6c94990c2e0b5735e4fdf3
chani <qkrcks0426@naver.com>
2023-06-28 15:32:00 +0900

    [ADD] CenterContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/CenterControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/CenterControllerTest.java
@@ -64,108 +64,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 class CenterControllerTest extends ResponseRequests {
 
-    @InjectMocks
-    CenterController target;
-    @Mock
-    CenterService centerService;
-    MockMvc mockMvc;
-    ObjectMapper objectMapper;
-
-    @BeforeEach
-    private void init(){
-        mockMvc = MockMvcBuilders.standaloneSetup(target)
-                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver(), new LoginUserArgumentResolver("secretKey"))
-                .setControllerAdvice(GlobalControllerAdvice.class)
-                .build();
-        objectMapper = new ObjectMapper();
-    }
-
-    public String createJwtToken(User user){
-        return JWT.create()
-                .withSubject("JWT")
-                .withExpiresAt(new Date(System.currentTimeMillis() + (60000 * 60 * 3))) // JWT 만료시간 밀리세컨단위
-                .withClaim("id", user.getId())
-                .sign(Algorithm.HMAC512("secretKey"));
-    }
 
     @Nested
     @DisplayName("센터_베너_정보_검색")
     class BannerControllerTest {
 
-        @Test
-        public void 센터_정보_검색_배너() throws Exception {
-            //given
-            CenterBannerResponse response = new CenterBannerResponse(1L, "test", true, true, 4.5, null,"testLocation", List.of(new String[]{"dfd", "fsdfs"}));
-            Parent parent = createParent(1L);
-            String jwtToken = createJwtToken(parent);
+        // TODO 센터_정보_검색_배너
 
-            doReturn(response)
-                    .when(centerService).findBannerById(1L, 1L);
 
-            //when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.get("/center/1/recruit")
-                            .header("Authorization", jwtToken)
-            );
 
-            //then
-            verify(centerService, times(1))
-                    .findBannerById(1L, 1L);
 
-            result.andDo(print())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(content().json(objectMapper.writeValueAsString(response)));
 
-        }
 
-        @Test
-        public void 베너_검색_성공_선생님으로_검색() throws Exception {
-            //given
-            CenterBannerResponse response = new CenterBannerResponse(1L, "test", true, true, 4.5, null,"testLocation", List.of(new String[]{"dfd", "fsdfs"}));
-            Teacher teacher = createTeacher(1L);
-            String jwtToken = createJwtToken(teacher);
 
-            doReturn(response)
-                    .when(centerService).findBannerById(1L, 1L);
 
-            //when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.get("/center/1/recruit")
-                            .header("Authorization", jwtToken)
-            );
 
-            //then
-            verify(centerService, times(1))
-                    .findBannerById(1L, 1L);
 
-            result.andDo(print())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(content().json(objectMapper.writeValueAsString(response)));
-        }
 
-        @Test
-        public void 센터_베너_정보_검색_비회원() throws Exception {
-            //given
-            CenterBannerResponse response = new CenterBannerResponse(1L, "test", true, true, 4.5, null,"testLocation", List.of(new String[]{"dfd", "fsdfs"}));
-//            Teacher teacher = createTeacher(1L);
-//            String jwtToken = createJwtToken(teacher);
 
-            doReturn(response)
-                    .when(centerService).findBannerById(1L, null);
 
-            //when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.get("/center/1/recruit")
-            );
 
-            //then
-            verify(centerService, times(1))
-                    .findBannerById(1L, null);
+        // TODO 베너_검색_성공_선생님으로_검색
 
-            result.andDo(print())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(content().json(objectMapper.writeValueAsString(response)));
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // TODO 센터_베너_정보_검색_비회원
+
 
     }
 
@@ -173,256 +109,103 @@ class CenterControllerTest extends ResponseRequests {
     @DisplayName("시설 수정")
     class 시설수정{
 
-        CenterDetailRequest wrongRequest;
-        CenterDetailRequest rightRequest;
-        MockMultipartFile multipartFile;
-        List<MockMultipartFile> multipartFileList = new ArrayList<>();
-        Center center;
-        Teacher waitingTeacher;
-        Teacher acceptTeacher;
-
-        @BeforeEach
-        void init() throws IOException {
-            String name = "162693895955046828.png";
-            Path path1 = Paths.get(new File("").getAbsolutePath() + '/' + name);
-            byte[] content = Files.readAllBytes(path1);
-            multipartFile = new MockMultipartFile(name, name, "image", content);
-            multipartFileList.add(multipartFile);
-            multipartFileList.add(multipartFile);
-
-            waitingTeacher = createTeacher(1L, center, Auth.TEACHER, Approval.WAITING);
-            acceptTeacher = createTeacher(1L, center, Auth.TEACHER, Approval.ACCEPT);
-            wrongRequest = new CenterDetailRequest();
-            rightRequest = CenterDetailRequest.builder()
-                    .name("test")
-                    .director("test 원장")
-                    .tel("test 전번")
-                    .address("주소")
-                    .estType("공립")
-//                    .sido("sido")
-//                    .sigungu("sigungu")
-                    .offerService("")
-                    .recruit(false)
-                    .theme(coding())
-                    .build();
-        }
-
-        @Test
-        @DisplayName("[error] 시설 요청 오류")
-        public void 시설요청오류() throws Exception {
-            //given
-
-            MockMultipartHttpServletRequestBuilder builder = MockMvcRequestBuilders.multipart("/center/1");
-            builder.with(new RequestPostProcessor() {
-                @Override
-                public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
-                    request.setMethod(HttpMethod.PATCH.toString());
-                    return request;
-                }
-            });
-
-            MockMultipartFile requestDto = new MockMultipartFile("requestDto", null,
-                    "application/json", objectMapper.writeValueAsString(wrongRequest).getBytes());
-
-            //when
-            ResultActions result = mockMvc.perform(
-                    builder.file("infoImages", multipartFile.getBytes())
-                            .file("profileImage", multipartFile.getBytes())
-                            .file(requestDto)
-                            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-            );
-
-            //then
-            result.andDo(print())
-                    .andExpect(status().isBadRequest());
-
-        }
-
-        @Test
-        @DisplayName("[error] 로그인 안하면 오류")
-        public void 로그인X오류() throws Exception {
-            MockMultipartHttpServletRequestBuilder builder = MockMvcRequestBuilders.multipart("/center/1");
-            builder.with(new RequestPostProcessor() {
-                @Override
-                public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
-                    request.setMethod(HttpMethod.PATCH.toString());
-                    return request;
-                }
-            });
-
-            //when
-            ResultActions result = mockMvc.perform(
-                    builder.content(objectMapper.writeValueAsString(rightRequest))
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .with(csrf())
-            );
-
-            //then
-            result.andDo(print())
-                    .andExpect(status().isForbidden())
-                    .andExpect(content().json(objectMapper.writeValueAsString(
-                            new ErrorResponse(HttpStatus.FORBIDDEN
-                                    , "인증된 사용자가 아닙니다")
-                    )));
-        }
-
-        @Test
-        @DisplayName("[error] 로그인은 했으나 선생님이 아닌경우")
-        public void 선생님이아닌경우() throws Exception {
-            MockMultipartHttpServletRequestBuilder builder = MockMvcRequestBuilders.multipart("/center/1");
-            builder.with(new RequestPostProcessor() {
-                @Override
-                public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
-                    request.setMethod(HttpMethod.PATCH.toString());
-                    return request;
-                }
-            });
-
-            Mockito.doThrow(new UserException(UserErrorResult.USER_NOT_EXIST))
-                    .when(centerService).modifyCenterInfo(any(Long.class), any(Long.class), any(CenterDetailRequest.class));
-
-            //when
-            ResultActions result = mockMvc.perform(
-                    builder.content(objectMapper.writeValueAsString(rightRequest))
-                            .header(HttpHeaders.AUTHORIZATION, createJwtToken(createParent(1L)))
-                            .contentType(MediaType.APPLICATION_JSON)
-            );
-
-            //then
-            result.andDo(print())
-                    .andExpect(status().isBadRequest())
-                    .andExpect(content().json(objectMapper.writeValueAsString(
-                            new ErrorResponse(HttpStatus.BAD_REQUEST
-                                    , UserErrorResult.USER_NOT_EXIST.getMessage())
-                    )));
-        }
-
-        @Test
-        @DisplayName("[error] 시설 수정 권한이 없는 경우")
-        public void 시설작성권한없음() throws Exception {
-            MockMultipartHttpServletRequestBuilder builder = MockMvcRequestBuilders.multipart("/center/1");
-            builder.with(new RequestPostProcessor() {
-                @Override
-                public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
-                    request.setMethod(HttpMethod.PATCH.toString());
-                    return request;
-                }
-            });
-
-            Mockito.doThrow(new CenterException(CenterErrorResult.AUTHENTICATION_FAILED))
-                    .when(centerService).modifyCenterInfo(any(Long.class), any(Long.class), any(CenterDetailRequest.class));
-
-            //when
-            ResultActions result = mockMvc.perform(
-                    builder.content(objectMapper.writeValueAsString(rightRequest))
-                            .header(HttpHeaders.AUTHORIZATION, createJwtToken(createParent(1L)))
-                            .contentType(MediaType.APPLICATION_JSON)
-            );
-
-            //then
-            result.andDo(print())
-                    .andExpect(status().isForbidden())
-                    .andExpect(content().json(objectMapper.writeValueAsString(
-                            new ErrorResponse(HttpStatus.FORBIDDEN
-                                    , CenterErrorResult.AUTHENTICATION_FAILED.getMessage())
-                    )));
-        }
-
-        @Test
-        @DisplayName("[error] 시설 주소가 잘못된 경우")
-        public void 시설주소가잘못된경우() throws Exception {
-            MockMultipartHttpServletRequestBuilder builder = MockMvcRequestBuilders.multipart("/center/1");
-            builder.with(new RequestPostProcessor() {
-                @Override
-                public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
-                    request.setMethod(HttpMethod.PATCH.toString());
-                    return request;
-                }
-            });
-
-            Mockito.doThrow(new CenterException(CenterErrorResult.CENTER_WRONG_ADDRESS))
-                    .when(centerService).modifyCenterInfo(any(Long.class), any(Long.class), any(CenterDetailRequest.class));
-
-            //when
-            ResultActions result = mockMvc.perform(
-                    builder.content(objectMapper.writeValueAsString(rightRequest))
-                            .header(HttpHeaders.AUTHORIZATION, createJwtToken(createParent(1L)))
-                            .contentType(MediaType.APPLICATION_JSON)
-            );
-
-            //then
-            result.andDo(print())
-                    .andExpect(status().isBadRequest())
-                    .andExpect(content().json(objectMapper.writeValueAsString(
-                            new ErrorResponse(HttpStatus.BAD_REQUEST
-                                    , CenterErrorResult.CENTER_WRONG_ADDRESS.getMessage())
-                    )));
-        }
-
-//
-
-        @Test
-        @DisplayName("[success] 시설 수정 성공")
-        public void 시설이미지수정성공_APP() throws Exception {
-            MockMultipartHttpServletRequestBuilder builder = MockMvcRequestBuilders.multipart("/center/1/image");
-            builder.with(new RequestPostProcessor() {
-                @Override
-                public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
-                    request.setMethod(HttpMethod.PATCH.toString());
-                    return request;
-                }
-            });
-
-            Mockito.doReturn(1L)
-                    .when(centerService).modifyCenterImage(any(Long.class), any(Long.class), anyList(), any(MultipartFile.class));
+        // TODO 시설 요청 오류
 
 
-            //when
-            ResultActions result = mockMvc.perform(
-                    builder.file("infoImages", multipartFile.getBytes())
-                            .file("profileImage", multipartFile.getBytes())
-                            .header(HttpHeaders.AUTHORIZATION, createJwtToken(createParent(1L)))
-                            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-            );
 
-            //then
-            result.andDo(print())
-                    .andExpect(status().isAccepted())
-                    .andExpect(content().json(objectMapper.writeValueAsString(
-                            1L
-                    )));
-        }
 
-        @Test
-        @DisplayName("[success] 시설 수정 성공")
-        public void 시설정보수정성공_APP() throws Exception {
-            MockMultipartHttpServletRequestBuilder builder = MockMvcRequestBuilders.multipart("/center/1");
-            builder.with(new RequestPostProcessor() {
-                @Override
-                public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
-                    request.setMethod(HttpMethod.PATCH.toString());
-                    return request;
-                }
-            });
 
-            Mockito.doReturn(1L)
-                    .when(centerService).modifyCenterInfo(any(Long.class), any(Long.class), any(CenterDetailRequest.class));
 
-            //when
-            ResultActions result = mockMvc.perform(
-                    builder
-                            .content(objectMapper.writeValueAsString(rightRequest))
-                            .header(HttpHeaders.AUTHORIZATION, createJwtToken(createParent(1L)))
-                            .contentType(MediaType.APPLICATION_JSON)
-            );
 
-            //then
-            result.andDo(print())
-                    .andExpect(status().isAccepted())
-                    .andExpect(content().json(objectMapper.writeValueAsString(
-                            1L
-                    )));
-        }
+
+
+
+
+
+
+
+        // TODO 로그인 안하면 오류
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // TODO 로그인은 했으나 선생님이 아닌경우
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // TODO 시설 수정 권한이 없는 경우
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // TODO 시설 주소가 잘못된 경우
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // TODO 시설이미지수정성공_APP
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // TODO 시설정보수정성공_APP
+
+
     }
 
 }
