dbc540ee7de57c89cbc9a11f7ac323331c8bc811
chani <qkrcks0426@naver.com>
2023-06-28 17:25:35 +0900

    [ADD] ExpoTokenContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/ExpoTokenControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/ExpoTokenControllerTest.java
@@ -1,36 +1,10 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.dto.expoToken.ExpoTokenRequest;
-import FIS.iLUVit.domain.Parent;
-import FIS.iLUVit.domain.User;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.ExpoTokenService;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
 @ExtendWith(MockitoExtension.class)
 class ExpoTokenControllerTest {
 
     // TODO ExpoTokenSave
-
 
 }
