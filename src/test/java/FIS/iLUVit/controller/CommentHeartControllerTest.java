7346cbe4fdf5af6c69527ea84ec0069e880f09fd
chani <qkrcks0426@naver.com>
2023-06-28 17:25:10 +0900

    [ADD] CommentHeartContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/CommentHeartControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/CommentHeartControllerTest.java
@@ -1,140 +1,23 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.Creator;
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.domain.*;
-import FIS.iLUVit.domain.enumtype.Auth;
-import FIS.iLUVit.domain.enumtype.BoardKind;
-import FIS.iLUVit.dto.comment.CommentDto;
-import FIS.iLUVit.dto.comment.CommentRequest;
-import FIS.iLUVit.exception.CommentErrorResult;
-import FIS.iLUVit.exception.CommentException;
-import FIS.iLUVit.exception.PostErrorResult;
-import FIS.iLUVit.exception.PostException;
-import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.CommentHeartService;
-import FIS.iLUVit.service.CommentService;
-import FIS.iLUVit.service.createmethod.CreateTest;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.Arrays;
-import java.util.Date;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class CommentHeartControllerTest {
 
     // TODO 댓글_작성_유저X
 
-
-
-
-
-
-
-
-
-
-
     // TODO 댓글_좋아요_비회원
-
-
-
-
-
-
-
-
-
-
-
 
     // TODO 댓글_좋아요_이미_존재
 
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 댓글_좋아요_성공
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // TODO 댓글_좋아요_취소_비회원_혹은_권한X
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 댓글_좋아요_취소_좋아요X
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 댓글_좋아요_취소_성공
-
-
 
 }
