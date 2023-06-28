dcbe3814c777b511fee5fca1a2f1e12ccbde88d4
chani <qkrcks0426@naver.com>
2023-06-28 17:34:15 +0900

    [ADD] ReviewHeartContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/ReviewHeartControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/ReviewHeartControllerTest.java
@@ -1,98 +1,23 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.Creator;
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.domain.*;
-import FIS.iLUVit.domain.enumtype.Approval;
-import FIS.iLUVit.domain.enumtype.Auth;
-import FIS.iLUVit.exception.ReviewErrorResult;
-import FIS.iLUVit.exception.ReviewException;
-import FIS.iLUVit.exception.UserErrorResult;
-import FIS.iLUVit.exception.UserException;
-import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.ReviewHeartService;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.Date;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class ReviewHeartControllerTest {
 
-
     // TODO 리뷰_좋아요_비회원
-
-
-
-
-
-
-
 
     // TODO 리뷰_좋아요_중복_등록
 
-
-
-
-
-
-
-
     // TODO 리뷰_좋아요_리뷰X
-
-
-
-
-
-
-
 
     // TODO 리뷰_좋아요_유저X
 
-
-
-
-
-
-
-
     // TODO 리뷰_좋아요_성공
-
-
-
-
-
-
-
 
     // TODO 리뷰_삭제_좋아요X
 
-
-
-
-
-
-
-
     // TODO 리뷰_삭제_성공
-
 
 }
