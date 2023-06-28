0b82fa9900ab8e5a892259dab8670a5bf1f6bd47
chani <qkrcks0426@naver.com>
2023-06-28 17:29:53 +0900

    [ADD] ReportContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/ReportControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/ReportControllerTest.java
@@ -1,123 +1,25 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.Creator;
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.dto.report.ReportRequest;
-import FIS.iLUVit.domain.Comment;
-import FIS.iLUVit.domain.Post;
-import FIS.iLUVit.domain.Teacher;
-import FIS.iLUVit.domain.User;
-import FIS.iLUVit.domain.enumtype.ReportReason;
-import FIS.iLUVit.domain.enumtype.ReportType;
-import FIS.iLUVit.domain.reports.Report;
-import FIS.iLUVit.domain.reports.ReportDetail;
-import FIS.iLUVit.exception.*;
-import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.ReportService;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.Date;
-
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class ReportControllerTest {
 
     // TODO 신고_실패_유저토큰무효
 
-
-
-
-
-
-
-
-
-
-
     // TODO 신고_실패_유저존재안함
-
-
-
-
-
-
-
-
-
 
     // TODO 게시글신고_실패_게시글존재안함
 
-
-
-
-
-
-
-
-
-
     // TODO 게시글신고_실패_중복신고
-
-
-
-
-
-
-
-
 
     // TODO 게시글신고_성공
 
-
-
-
-
-
-
-
-
     // TODO 댓글신고_실패_댓글존재안함
-
-
-
-
-
-
-
-
 
     // TODO 댓글신고_실패_중복신고
 
-
-
-
-
-
-
-
-
     // TODO 댓글신고_성공
-
 
 }
