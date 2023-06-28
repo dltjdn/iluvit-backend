327761f0ba647f8c68a39ca8f50bce17bfe7f0fc
chani <qkrcks0426@naver.com>
2023-06-28 17:21:01 +0900

    [ADD] CommentHeartContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/CenterControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/CenterControllerTest.java
@@ -1,69 +1,13 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.dto.center.CenterBannerResponse;
-import FIS.iLUVit.dto.center.CenterDetailRequest;
 import FIS.iLUVit.controller.messagecreate.ResponseRequests;
-import FIS.iLUVit.domain.Center;
-import FIS.iLUVit.domain.Parent;
-import FIS.iLUVit.domain.Teacher;
-import FIS.iLUVit.domain.User;
-import FIS.iLUVit.domain.enumtype.Approval;
-import FIS.iLUVit.domain.enumtype.Auth;
-import FIS.iLUVit.exception.CenterErrorResult;
-import FIS.iLUVit.exception.CenterException;
-import FIS.iLUVit.exception.UserErrorResult;
-import FIS.iLUVit.exception.UserException;
-import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.CenterService;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import static FIS.iLUVit.Creator.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @ExtendWith(MockitoExtension.class)
 class CenterControllerTest extends ResponseRequests {
-
 
     @Nested
     @DisplayName("센터_베너_정보_검색")
@@ -71,37 +15,9 @@ class CenterControllerTest extends ResponseRequests {
 
         // TODO 센터_정보_검색_배너
 
-
-
-
-
-
-
-
-
-
-
-
-
-
         // TODO 베너_검색_성공_선생님으로_검색
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         // TODO 센터_베너_정보_검색_비회원
-
 
     }
 
@@ -111,100 +27,17 @@ class CenterControllerTest extends ResponseRequests {
 
         // TODO 시설 요청 오류
 
-
-
-
-
-
-
-
-
-
-
-
-
-
         // TODO 로그인 안하면 오류
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         // TODO 로그인은 했으나 선생님이 아닌경우
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         // TODO 시설 수정 권한이 없는 경우
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         // TODO 시설 주소가 잘못된 경우
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         // TODO 시설이미지수정성공_APP
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         // TODO 시설정보수정성공_APP
-
 
     }
 
