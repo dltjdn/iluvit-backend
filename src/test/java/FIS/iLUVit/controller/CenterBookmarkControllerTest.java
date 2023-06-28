b812cdfe381ac4c46ee444bd5f8a0e5d28c2cfa8
chani <qkrcks0426@naver.com>
2023-06-28 17:19:54 +0900

    [ADD] CenterBookmarkContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/CenterBookmarkControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/CenterBookmarkControllerTest.java
@@ -1,95 +1,24 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.Creator;
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.domain.Center;
-import FIS.iLUVit.domain.Parent;
-import FIS.iLUVit.domain.Prefer;
-import FIS.iLUVit.domain.User;
-import FIS.iLUVit.domain.enumtype.Auth;
-import FIS.iLUVit.exception.PreferErrorResult;
-import FIS.iLUVit.exception.PreferException;
-import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.CenterBookmarkService;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.Date;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class CenterBookmarkControllerTest {
 
     // TODO 즐겨찾기한 시설 목록 조회 성공
 
-
-
-
-
-
-
-
-
-
-
-
-
     @Nested
     @DisplayName("시설 즐겨찾기")
     class savePrefer{
 
-
         // TODO 이미 즐겨찾기한 시설
-
-
-
-
-
-
-
-
-
-
-
-
 
         // TODO 잘못된 시설 (centerIdError)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         // TODO 시설_즐겨찾기_성공
-
-
 
     }
 
@@ -99,24 +28,7 @@ class CenterBookmarkControllerTest {
 
         // TODO 시설_정보_오류 (유효하지 않은 시설정보)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         // TODO 시설_즐겨찾기_해제성공 (유효하지 않은 시설정보)
-
-
-
 
     }
 
