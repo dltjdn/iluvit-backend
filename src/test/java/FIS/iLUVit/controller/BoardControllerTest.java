1af37a568305fc7cae31ccfa53dba7de96cc9421
chani <qkrcks0426@naver.com>
2023-06-28 17:18:46 +0900

    [ADD] BoardContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/BoardControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/BoardControllerTest.java
@@ -1,153 +1,23 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.dto.board.BoardListDto;
-import FIS.iLUVit.dto.board.BoardRequest;
-import FIS.iLUVit.dto.board.StoryPreviewDto;
-import FIS.iLUVit.domain.Board;
-import FIS.iLUVit.domain.Center;
-import FIS.iLUVit.domain.Parent;
-import FIS.iLUVit.domain.User;
-import FIS.iLUVit.domain.enumtype.BoardKind;
-import FIS.iLUVit.exception.UserErrorResult;
-import FIS.iLUVit.exception.UserException;
-import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.BoardService;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.http.MediaType;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class BoardControllerTest {
 
     // TODO 게시판_생성
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 게시판_삭제
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // TODO 모두의_이야기_내_게시판_목록_조회
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 시설_유치원_내_게시판_목록_조회
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // TODO 이야기_홈에서_센터_게시판_띄워주기_비회원
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 이야기_홈에서_센터_게시판_띄워주기_유저X
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 이야기_홈에서_센터_게시판_띄워주기_성공
-
-
-
-
 
 }
