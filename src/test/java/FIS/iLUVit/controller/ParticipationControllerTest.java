d96a227917b426e8d5f98ead96b69d2687009037
chani <qkrcks0426@naver.com>
2023-06-28 17:27:08 +0900

    [ADD] ParticipationContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/ParticipationControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/ParticipationControllerTest.java
@@ -1,105 +1,24 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.dto.presentation.PtDateRequest;
-import FIS.iLUVit.domain.Parent;
-import FIS.iLUVit.domain.User;
-import FIS.iLUVit.domain.enumtype.Auth;
-import FIS.iLUVit.exception.ParticipationErrorResult;
-import FIS.iLUVit.exception.ParticipationException;
-import FIS.iLUVit.exception.PresentationErrorResult;
-import FIS.iLUVit.exception.PresentationException;
-import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.ParticipationService;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.Date;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class ParticipationControllerTest {
 
     // TODO 설명회_신청_로그인안함
 
-
-
-
-
-
-
-
     // TODO 설명회_신청_설명회_회차_아이디값_음수
-
-
-
-
-
-
-
 
     // TODO 설명회_수용인원_초과
 
-
-
-
-
-
-
-
-
     // TODO 설명회_신청_이미_신청한_사용자
-
-
-
-
-
-
-
-
-
 
     // TODO 설명회_신청기간_지남
 
-
-
-
-
-
-
-
-
-
     // TODO 설명회_신청_성공
-
-
-
-
-
-
 
     @Nested
     @DisplayName("설명회 취소")
@@ -107,38 +26,11 @@ class ParticipationControllerTest {
 
         // TODO 로그인 안함
 
-
-
-
-
-
-
-
-
         // TODO 잘못된_participationId_요청 (잘못 요청시 오류 발생)
-
-
-
-
-
-
-
-
 
         // TODO 잘못된 요청으로 service 오류 발생
 
-
-
-
-
-
-
-
-
         // TODO 설명회 취소 성공
 
-
-
     }
-
 }
