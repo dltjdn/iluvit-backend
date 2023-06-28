e9186219dfb0372f6f80bcc5ced4c6bbe1f306e5
chani <qkrcks0426@naver.com>
2023-06-28 17:24:25 +0900

    [ADD] CommentContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/CommentControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/CommentControllerTest.java
@@ -1,163 +1,27 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.Creator;
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.dto.comment.CommentDto;
-import FIS.iLUVit.dto.comment.CommentRequest;
-import FIS.iLUVit.domain.*;
-import FIS.iLUVit.domain.enumtype.Auth;
-import FIS.iLUVit.domain.enumtype.BoardKind;
-import FIS.iLUVit.exception.CommentErrorResult;
-import FIS.iLUVit.exception.CommentException;
-import FIS.iLUVit.exception.PostErrorResult;
-import FIS.iLUVit.exception.PostException;
-import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.CommentService;
-import FIS.iLUVit.service.createmethod.CreateTest;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.SliceImpl;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.Arrays;
-import java.util.Date;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class CommentControllerTest {
 
     // TODO 댓글_작성_비회원_접근
 
-
-
-
-
-
-
-
-
-
-
     // TODO 댓글_작성_유저X
-
-
-
-
-
-
-
-
-
-
 
     // TODO 댓글_작성_게시글X
 
-
-
-
-
-
-
-
-
-
-
-
     // TODO 댓글_작성_성공
-
-
-
-
-
-
-
-
-
-
-
-
 
     // TODO 대댓글_작성_성공
 
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 댓글_삭제_비회원_혹은_접근_권한_제한
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // TODO 댓글_삭제_댓글X
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 댓글_삭제_성공
 
-
-
-
-
-
-
-
-
-
-
-
-
     // TODO 댓글_단_리스트_조회
-
-
 
 }
