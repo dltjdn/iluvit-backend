79688093b086acba812aedcbd738d25581224180
chani <qkrcks0426@naver.com>
2023-06-28 17:17:50 +0900

    [ADD] BoardBookmarkContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/BoardBookmarkControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/BoardBookmarkControllerTest.java
@@ -1,42 +1,7 @@
 package FIS.iLUVit.controller;
 
-import FIS.iLUVit.config.argumentResolver.LoginUserArgumentResolver;
-import FIS.iLUVit.dto.board.StoryDto;
-import FIS.iLUVit.domain.Board;
-import FIS.iLUVit.domain.Bookmark;
-import FIS.iLUVit.domain.Parent;
-import FIS.iLUVit.domain.User;
-import FIS.iLUVit.domain.enumtype.Auth;
-import FIS.iLUVit.domain.enumtype.BoardKind;
-import FIS.iLUVit.exception.BookmarkErrorResult;
-import FIS.iLUVit.exception.BookmarkException;
-import FIS.iLUVit.exception.exceptionHandler.ErrorResponse;
-import FIS.iLUVit.exception.exceptionHandler.controllerAdvice.GlobalControllerAdvice;
-import FIS.iLUVit.service.BoardBookmarkService;
-import FIS.iLUVit.service.createmethod.CreateTest;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class BoardBookmarkControllerTest {
