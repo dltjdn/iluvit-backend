f93d3ea47a2b68e69e0636b665192812ed2245d6
chani <qkrcks0426@naver.com>
2023-06-28 14:32:10 +0900

    [ADD] AuthContollerTest 완료 #304

--- src/test/java/FIS/iLUVit/controller/AuthControllerTest.java
+++ src/test/java/FIS/iLUVit/controller/AuthControllerTest.java
@@ -39,405 +39,280 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 public class AuthControllerTest {
 
-    @Mock
-    private AuthService authService;
-    @InjectMocks
-    private AuthController target;
+    // TODO 회원가입인증번호받기_실패_이미가입된번호
 
-    private MockMvc mockMvc;
-    private ObjectMapper objectMapper;
-    private User user;
-    private final String phoneNum = "phoneNumber";
-    private final String authNum = "authNumber";
 
-    @BeforeEach
-    public void init() {
-        objectMapper = new ObjectMapper();
-        mockMvc = MockMvcBuilders.standaloneSetup(target)
-                .setCustomArgumentResolvers(new LoginUserArgumentResolver("secretKey"))
-                .setControllerAdvice(GlobalControllerAdvice.class)
-                .build();
-        user = Parent.builder()
-                .id(1L)
-                .phoneNumber(phoneNum)
-                .name("parent")
-                .build();
-    }
 
-    public String createJwtToken(User user){
-        return JWT.create()
-                .withSubject("JWT")
-                .withExpiresAt(new Date(System.currentTimeMillis() + (60000 * 60 * 3))) // JWT 만료시간 밀리세컨단위
-                .withClaim("id", user.getId())
-                .sign(Algorithm.HMAC512("secretKey"));
-    }
 
-    @Test
-    public void 회원가입인증번호받기_실패_이미가입된번호() throws Exception {
-        // given
-        final String url = "/auth/signup";
-        AuthNumberErrorResult error = AuthNumberErrorResult.ALREADY_PHONENUMBER_REGISTER;
 
-        doThrow(new AuthNumberException(error))
-                .when(authService)
-                .sendAuthNumberForSignup(phoneNum);
 
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
-                        .param("phoneNumber", phoneNum)
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
 
-        // then
-        resultActions.andDo(print())
-                .andExpect(status().isBadRequest())
-                .andExpect(
-                        content().json(objectMapper.writeValueAsString(
-                                new ErrorResponse(error.getHttpStatus(), error.getMessage())))
-                );
-    }
 
-    @Test
-    public void 인증번호받기_실패_유효시간남음() throws Exception {
-        // given
-        final String url = "/auth/signup";
-        AuthNumberErrorResult error = AuthNumberErrorResult.YET_AUTHNUMBER_VALID;
 
-        doThrow(new AuthNumberException(error))
-                .when(authService)
-                .sendAuthNumberForSignup(phoneNum);
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
-                        .param("phoneNumber", phoneNum)
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andDo(print())
-                .andExpect(status().isBadRequest())
-                .andExpect(
-                        content().json(objectMapper.writeValueAsString(
-                                new ErrorResponse(error.getHttpStatus(), error.getMessage())
-                        ))
-                );
-    }
 
-    @Test
-    public void 인증번호받기_성공() throws Exception {
-        // given
-        final String url = "/auth/signup";
 
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
-                        .param("phoneNumber", phoneNum)
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andExpect(status().isOk());
-    }
 
-    @Test
-    public void 인증번호인증_실패_인증정보불일치() throws Exception {
-        // given
-        final String url = "/auth";
-        AuthNumRequest request = new AuthNumRequest(phoneNum, authNum, AuthKind.signup);
-        AuthNumberErrorResult error = AuthNumberErrorResult.AUTHENTICATION_FAIL;
-        doThrow(new AuthNumberException(error))
-                .when(authService)
-                .authenticateAuthNum(null, request);
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andDo(print())
-                .andExpect(status().isBadRequest())
-                .andExpect(content().json(
-                        objectMapper.writeValueAsString(
-                                new ErrorResponse(error.getHttpStatus(), error.getMessage())))
-                );
-    }
+    // TODO 인증번호받기_실패_유효시간남음
 
-    @Test
-    public void 인증번호인증_실패_인증번호만료() throws Exception {
-        // given
-        final String url = "/auth";
-        AuthNumRequest request = new AuthNumRequest(phoneNum, authNum, AuthKind.signup);
-        AuthNumberErrorResult error = AuthNumberErrorResult.EXPIRED;
-        doThrow(new AuthNumberException(error))
-                .when(authService)
-                .authenticateAuthNum(null, request);
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andDo(print())
-                .andExpect(status().isBadRequest())
-                .andExpect(content().json(
-                        objectMapper.writeValueAsString(
-                                new ErrorResponse(error.getHttpStatus(), error.getMessage())))
-                );
 
-    }
 
-    @Test
-    public void 인증번호인증_성공() throws Exception {
-        // given
-        final String url = "/auth";
-        AuthNumRequest request = new AuthNumRequest(phoneNum, authNum, AuthKind.signup);
 
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andDo(print())
-                .andExpect(status().isOk());
-    }
 
-    @Test
-    public void 인증번호인증_성공_핸드폰변경() throws Exception {
-        // given
-        final String url = "/auth";
-        AuthNumRequest request = new AuthNumRequest(phoneNum, authNum, AuthKind.updatePhoneNum);
 
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .header("Authorization", createJwtToken(user))
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andExpect(status().isOk());
-    }
 
-    @Test
-    public void 아이디찾기인증번호받기_실패_가입하지않은핸드폰() throws Exception {
-        // given
-        final String url = "/auth/loginid";
-        AuthNumberErrorResult error = AuthNumberErrorResult.NOT_SIGNUP_PHONE;
-        doThrow(new AuthNumberException(error))
-                .when(authService)
-                .sendAuthNumberForFindLoginId(phoneNum);
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
-                        .param("phoneNumber", phoneNum)
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andExpect(status().isBadRequest())
-                .andExpect(content().json(
-                        objectMapper.writeValueAsString(
-                                new ErrorResponse(error.getHttpStatus(), error.getMessage())))
-                );
-    }
 
-    @Test
-    public void 아이디찾기인증번호받기_성공() throws Exception {
-        // given
-        final String url = "/auth/loginid";
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
-                        .param("phoneNumber", phoneNum)
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andExpect(status().isOk());
-    }
 
-    @Test
-    public void 아이디찾기_성공() throws Exception {
-        // given
-        Gson gson = new Gson();
-        String url = "/auth/loginid";
-        String loginId = "lo***Id";
-        AuthNumRequest request = new AuthNumRequest(phoneNum, authNum, AuthKind.findLoginId);
-        doReturn(loginId)
-                .when(authService)
-                .findLoginId(request);
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post(url)
-                        .content(objectMapper.writeValueAsString(request))
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(loginId));
-    }
 
-    @Test
-    public void 비밀번호찾기인증번호받기_실패_아이디휴대폰불일치() throws Exception {
-        // given
-        String url = "/auth/password";
-        AuthNumberErrorResult error = AuthNumberErrorResult.NOT_MATCH_INFO;
-        doThrow(new AuthNumberException(error))
-                .when(authService)
-                .sendAuthNumberForFindPassword("loginId", "phoneNumber");
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
-                        .param("loginId", "loginId")
-                        .param("phoneNumber", "phoneNumber")
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andDo(print())
-                .andExpect(status().isBadRequest())
-                .andExpect(content().json(
-                        objectMapper.writeValueAsString(
-                                new ErrorResponse(error.getHttpStatus(), error.getMessage())))
-                );
-    }
 
-    @Test
-    public void 비밀번호찾기인증번호받기_성공() throws Exception {
-        // given
-        String url = "/auth/password";
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
-                        .param("loginId", "loginId")
-                        .param("phoneNumber", "phoneNumber")
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andExpect(status().isOk());
-    }
 
-    @Test
-    public void 핸드폰변경을위한인증번호받기_실패_토큰없음() throws Exception {
-        // given
-        String url = "/auth/phonenumber";
-        AuthNumberErrorResult error = AuthNumberErrorResult.AUTHENTICATION_FAIL;
-        doThrow(new AuthNumberException(error))
-                .when(authService)
-                .sendAuthNumberForChangePhone(null, phoneNum);
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
-                        .param("phoneNumber", phoneNum)
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andExpect(status().isBadRequest())
-                .andExpect(content().json(
-                        objectMapper.writeValueAsString(new ErrorResponse(error.getHttpStatus(), error.getMessage()))
-                ));
-    }
 
-    @Test
-    public void 핸드폰변경을위한인증번호받기_성공() throws Exception {
-        // given
-        String url = "/auth/phonenumber";
-        // when
-        ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.get(url)
-                        .header("Authorization", createJwtToken(user))
-                        .param("phoneNumber", phoneNum)
-                        .contentType(MediaType.APPLICATION_JSON)
-        );
-        // then
-        resultActions.andExpect(status().isOk());
-    }
+    // TODO 인증번호받기_성공
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 인증번호인증_실패_인증정보불일치
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 인증번호인증_실패_인증번호만료
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 인증번호인증_성공
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 인증번호인증_성공_핸드폰변경
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 아이디찾기인증번호받기_실패_가입하지않은핸드폰
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 아이디찾기인증번호받기_성공
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 아이디찾기_성공
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 비밀번호찾기인증번호받기_실패_아이디휴대폰불일치
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 비밀번호찾기인증번호받기_성공
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 핸드폰변경을위한인증번호받기_실패_토큰없음
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 핸드폰변경을위한인증번호받기_성공
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO 핸드폰변경을위한인증번호받기_성공
 
     @Nested
     @DisplayName("비밀번호 찾기")
     class findPassword{
-        @Test
-        @DisplayName("[error] 비밀번호 조건 불만족")
-        public void 비번확인틀림() throws Exception {
-            // given
-            String url = "/auth/password";
-            FindPasswordRequest request = new FindPasswordRequest(user.getLoginId(), user.getPhoneNumber(), "1234", "asd", "asd");
-            // when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.post(url)
-                            .header("Authorization", Creator.createJwtToken(user))
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request))
-            );
-            // then
-            result.andDo(print())
-                    .andExpect(status().isBadRequest());
-        }
 
-        @Test
-        @DisplayName("[error] 비밀번호확인 불일치")
-        public void 비번확인불일치() throws Exception {
-            // given
-            String url = "/auth/password";
-            AuthNumberErrorResult error = AuthNumberErrorResult.NOT_MATCH_CHECKPWD;
-            FindPasswordRequest request = new FindPasswordRequest(user.getLoginId(), user.getPhoneNumber(), "1234", "asdf1234!", "asdf12345!");
-            doThrow(new AuthNumberException(error))
-                    .when(authService)
-                    .changePassword(any());
-            // when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.post(url)
-                            .header("Authorization", Creator.createJwtToken(user))
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request)));
-            // then
-            result.andExpect(status().isBadRequest())
-                    .andExpect(content().json(
-                            objectMapper.writeValueAsString(new ErrorResponse(error.getHttpStatus(), error.getMessage()))
-                    ));
-        }
+        // TODO 비밀번호 조건 불만족
 
-        @Test
-        @DisplayName("[error] 사용자 정보 불일치")
-        public void 사용자정보불일치() throws Exception {
-            // given
-            String url = "/auth/password";
-            AuthNumberErrorResult error = AuthNumberErrorResult.NOT_MATCH_INFO;
-            FindPasswordRequest request = new FindPasswordRequest(user.getLoginId(), user.getPhoneNumber(), "1234", "asdf1234!", "asdf1234!");
-            doThrow(new AuthNumberException(error))
-                    .when(authService)
-                    .changePassword(any());
-            // when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.post(url)
-                            .header("Authorization", Creator.createJwtToken(user))
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request))
-            );
-            // then
-            result.andExpect(status().isBadRequest())
-                    .andExpect(content().json(
-                            objectMapper.writeValueAsString(new ErrorResponse(error.getHttpStatus(), error.getMessage()))
-                    ));
-        }
 
-        @Test
-        @DisplayName("[success] 비밀번호찾기 완료")
-        public void 비번찾기완료() throws Exception {
-            // given
-            String url = "/auth/password";
-            FindPasswordRequest request = new FindPasswordRequest(user.getLoginId(), user.getPhoneNumber(), "1234", "asdf1234!", "asdf1234!");
-            // when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.post(url)
-                            .header("Authorization", Creator.createJwtToken(user))
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(request))
-            );
-            // then
-            result.andExpect(status().isOk());
-        }
+
+
+
+
+
+
+
+
+
+
+        // TODO 비밀번호확인 불일치
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // TODO 사용자 정보 불일치
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // TODO 비밀번호찾기 완료
+
     }
 
 }
