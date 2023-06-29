e66bbe227cdaff37869fb6a32c0df532547c6b8f
chani <qkrcks0426@naver.com>
2023-06-29 16:07:44 +0900

    [DELETE] CenterBookmarkRepositoryTest 완료 #304

--- src/test/java/FIS/iLUVit/repository/CenterBookmarkRepositoryTest.java
+++ src/test/java/FIS/iLUVit/repository/CenterBookmarkRepositoryTest.java
@@ -18,44 +18,6 @@ import static org.assertj.core.api.Assertions.*;
 @DataJpaTest(includeFilters = @ComponentScan.Filter(ForDB.class))
 public class CenterBookmarkRepositoryTest {
 
-    @Autowired
-    private CenterBookmarkRepository centerBookmarkRepository;
-
-    @Autowired
-    private EntityManager em;
-
-    private Center center1;
-    private Center center2;
-    private Parent parent1;
-    private Parent parent2;
-    private Prefer prefer1;
-
-    @BeforeEach
-    public void init() {
-        center1 = Creator.createCenter("center1");
-        center2 = Creator.createCenter("center2");
-        parent1 = Creator.createParent("parent1", "phoneNum1");
-        parent2 = Creator.createParent("parent2", "phoneNum2");
-        prefer1 = Creator.createPrefer(parent1, center1);
-        em.persist(center1);
-        em.persist(center2);
-        em.persist(parent1);
-        em.persist(parent2);
-        em.persist(prefer1);
-    }
-
-    @Test
-    public void findByUserIdAndCenterId() {
-        // given
-        em.flush();
-        em.clear();
-        // when
-        Prefer result = centerBookmarkRepository.findByUserIdAndCenterId(parent1.getId(), center1.getId())
-                .orElse(null);
-        // then
-        assertThat(result).isNotNull();
-        assertThat(result.getCenter().getId()).isEqualTo(center1.getId());
-        assertThat(result.getParent().getId()).isEqualTo(parent1.getId());
-    }
+    // TODO find By User Id And Center Id
 
 }
