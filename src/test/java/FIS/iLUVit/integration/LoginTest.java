407db46ed195cff93e89e919bdcd7c76071d0f97
chani <qkrcks0426@naver.com>
2023-06-29 13:32:22 +0900

    [ADD] LoginTest 완료 #304

--- src/test/java/FIS/iLUVit/integration/LoginTest.java
+++ src/test/java/FIS/iLUVit/integration/LoginTest.java
@@ -47,211 +47,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class LoginTest {
 
-    @Value("${security.secretKey}")
-    private String secretKey;
-    @Autowired
-    private UserRepository userRepository;
-    @SpyBean
-    private TokenPairRepository tokenPairRepository;
-
-    @Autowired
-    private UserService userService;
-    @SpyBean
-    private JwtUtils jwtUtils;
-
-    @Autowired
-    private BCryptPasswordEncoder encoder;
-    @Autowired
-    private MockMvc mockMvc;
-
-    private ObjectMapper objectMapper;
-
-    private String loginId;
-    private String password;
-    private Parent parent;
-
-    @BeforeEach
-    public void init() {
-        objectMapper = new ObjectMapper();
-
-        loginId = "loginId";
-        password = "password";
-        parent = Parent.builder()
-                .loginId(loginId)
-                .password(encoder.encode(password))
-                .build();
-    }
-
     @Nested
     @DisplayName("로그인")
     class login {
-        @Test
-        @DisplayName("[error] 로그인아이디 틀림")
-        public void 로그인아이디틀림() throws Exception {
-            // given
-            userRepository.save(parent);
-            String url = "/login";
-            LoginRequest request = new LoginRequest("!@#!QAZ", password);
-            // when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.post(url)
-                            .content(objectMapper.writeValueAsString(request))
-                            .contentType(MediaType.APPLICATION_JSON)
-            );
-            // then
-            result.andDo(print())
-                    .andExpect(status().isUnauthorized());
-        }
 
-        @Test
-        @DisplayName("[error] 비밀번호 틀림")
-        public void 비밀번호틀림() throws Exception {
-            // given
-            userRepository.save(parent);
-            String url = "/login";
-            LoginRequest request = new LoginRequest(loginId, "!@#QAZ");
-            // when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.post(url)
-                            .content(objectMapper.writeValueAsString(request))
-                            .contentType(MediaType.APPLICATION_JSON)
-            );
-            // then
-            result.andDo(print())
-                    .andExpect(status().isUnauthorized());
-        }
+        // TODO 로그인아이디 틀림
 
-        @Test
-        @DisplayName("[success] 최초 로그인 성공(튜토리얼 진행)")
-        public void 최초로그인성공() throws Exception {
-            // given
-            userRepository.save(parent);
-            String url = "/login";
-            LoginRequest request = new LoginRequest(loginId, password);
-            // when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.post(url)
-                            .content(objectMapper.writeValueAsString(request))
-                            .contentType(MediaType.APPLICATION_JSON)
-            );
-            // then
-            User user = userRepository.findByLoginId(loginId).get();
-            TokenPair tokenPair = tokenPairRepository.findByUserId(user.getId()).get();
-            LoginResponse response = user.getLoginInfo();
-            response.setAccessToken("Bearer " + tokenPair.getAccessToken());
-            response.setRefreshToken("Bearer " + tokenPair.getRefreshToken());
-            response.setNeedTutorial(true);
-            result.andDo(print())
-                    .andExpect(status().isOk())
-                    .andExpect(content().json(
-                            objectMapper.writeValueAsString(response)
-                    ));
-            assertThat(response.getNeedTutorial()).isTrue();
-        }
+        // TODO 비밀번호 틀림
 
-        @Test
-        @DisplayName("[success] 로그인 성공(튜토리얼 진행 X)")
-        public void 로그인성공() throws Exception {
-            // given
-            userRepository.save(parent);
-            String url = "/login";
-            LoginRequest request = new LoginRequest(loginId, password);
-            parent.setUpdateDateForTest(LocalDateTime.now());
-            // when
-            ResultActions result = mockMvc.perform(
-                    MockMvcRequestBuilders.post(url)
-                            .content(objectMapper.writeValueAsString(request))
-                            .contentType(MediaType.APPLICATION_JSON)
-            );
-            // then
-            User user = userRepository.findByLoginId(loginId).get();
-            TokenPair tokenPair = tokenPairRepository.findByUserId(user.getId()).get();
-            LoginResponse response = user.getLoginInfo();
-            response.setAccessToken("Bearer " + tokenPair.getAccessToken());
-            response.setRefreshToken("Bearer " + tokenPair.getRefreshToken());
-            result.andDo(print())
-                    .andExpect(status().isOk())
-                    .andExpect(content().json(
-                            objectMapper.writeValueAsString(response)
-                    ));
-            assertThat(response.getNeedTutorial()).isFalse();
-        }
+        // TODO 최초 로그인 성공(튜토리얼 진행)
+
+        // TODO 로그인 성공(튜토리얼 진행 X)
     }
 
     @Nested
     @DisplayName("RefreshToken를 이용한 토큰 재발급")
     class refresh {
-        @Test
-        @DisplayName("[error] RefreshToken 유효성검사 실패")
-        public void 유효성검사실패() {
-            // given
-            userRepository.save(parent);
-            LoginResponse loginResponse = userService.login(new LoginRequest(loginId, password));
-            TokenRefreshRequest request = new TokenRefreshRequest(loginResponse.getRefreshToken() + "trash");
-            // when
-            assertThrows(JWTVerificationException.class,
-                    () -> userService.refreshAccessToken(request));
-            // then
-        }
 
-        @Test
-        @DisplayName("[error] DB와 불일치")
-        public void db와다름() {
-            // given
-            userRepository.save(parent);
-            LoginResponse loginResponse = userService.login(new LoginRequest(loginId, password));
-            doReturn(Optional.empty())
-                    .when(tokenPairRepository)
-                    .findByUserIdWithUser(any());
-            TokenRefreshRequest request = new TokenRefreshRequest(loginResponse.getRefreshToken());
-            // when
-            assertThrows(JWTVerificationException.class,
-                    () -> userService.refreshAccessToken(request));
-            // then
-        }
+        // TODO RefreshToken 유효성검사 실패
 
-        @Test
-        @DisplayName("[error] AccessToken 유효기간 남음")
-        public void 유효기간남음() {
-            // given
-            userRepository.save(parent);
-            LoginResponse loginResponse = userService.login(new LoginRequest(loginId, password));
-            TokenRefreshRequest request = new TokenRefreshRequest(loginResponse.getRefreshToken());
-            // when
-            LoginResponse result = userService.refreshAccessToken(request);
-            // then
-            assertThat(result).isNull();
-        }
+        // TODO DB와 불일치
 
-        @Test
-        @DisplayName("[success] 토큰 갱신 성공")
-        public void 토큰갱신성공() {
-            // given
-            userRepository.save(parent);
-            doReturn(createMockAccessToken(parent))
-                    .when(jwtUtils)
-                    .createAccessToken(any());
-            LoginResponse loginResponse = userService.login(new LoginRequest(loginId, password));
-            TokenRefreshRequest request = new TokenRefreshRequest(loginResponse.getRefreshToken());
-            // when
-            LoginResponse result = userService.refreshAccessToken(request);
-            // then
-            TokenPair tokenPair = tokenPairRepository.findByUserId(parent.getId()).orElse(null);
-            String dbAccessToken = tokenPair.getAccessToken();
-            String dbRefreshToken = tokenPair.getRefreshToken();
-            assertThat(result.getAccessToken()).isEqualTo(jwtUtils.addPrefix(dbAccessToken));
-            assertThat(result.getRefreshToken()).isEqualTo(jwtUtils.addPrefix(dbRefreshToken));
-        }
+        // TODO AccessToken 유효기간 남음
+
+        // TODO 토큰 갱신 성공
     }
-
-    public String createMockAccessToken(User user) {
-        String token = JWT.create()
-                .withSubject("ILuvIt_AccessToken")
-                .withExpiresAt(new Date(System.currentTimeMillis()-1000))
-                .withClaim("id", user.getId())
-                .sign(Algorithm.HMAC512(secretKey));
-        return token;
-    }
-
 
 }
