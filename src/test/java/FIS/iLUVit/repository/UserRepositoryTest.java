c618eff87f3ca7fc376fbb308f50292880048973
chani <qkrcks0426@naver.com>
2023-06-30 13:28:32 +0900

    [DELETE] UserRepositoryTest 완료 #304

--- src/test/java/FIS/iLUVit/repository/UserRepositoryTest.java
+++ src/test/java/FIS/iLUVit/repository/UserRepositoryTest.java
@@ -1,133 +1,17 @@
 package FIS.iLUVit.repository;
 
-import FIS.iLUVit.Creator;
 import FIS.iLUVit.config.argumentResolver.ForDB;
-import FIS.iLUVit.domain.Parent;
-import FIS.iLUVit.domain.Teacher;
-import FIS.iLUVit.domain.User;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 
-import javax.persistence.EntityManager;
-import java.util.Optional;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.predicate;
-
 @DataJpaTest(includeFilters = @ComponentScan.Filter(ForDB.class))
 public class UserRepositoryTest {
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private EntityManager em;
 
-    private User parent1;
-    private User parent2;
-    private User teacher1;
-    private User teacher2;
+    // TODO 이미 가입된 전화번호인지 확인
 
-    @BeforeEach
-    public void init() {
-        parent1 = Parent.builder()
-                .phoneNumber("parent1")
-                .loginId("parent1")
-                .nickName("parent1")
-                .build();
-        parent2 = Parent.builder()
-                .phoneNumber("parent2")
-                .loginId("parent2")
-                .nickName("parent2")
-                .build();
-        teacher1 = Teacher.builder()
-                .phoneNumber("teacher1")
-                .loginId("teacher1")
-                .build();
-        teacher2 = Teacher.builder()
-                .phoneNumber("teacher2")
-                .loginId("teacher2")
-                .build();
-    }
+    // TODO 로그인 아이디와 휴대폰 번호로 유저 조회
 
-    @Test
-    public void 이미가입된전화번호인지확인() {
-        // given
-        userRepository.save(parent1);
-        userRepository.save(parent2);
-        userRepository.save(teacher1);
-        userRepository.save(teacher2);
-        em.flush();
-        em.clear();
-        // when
-        User findUser = userRepository.findByPhoneNumber(parent1.getPhoneNumber()).orElse(null);
-        // then
-        assertThat(findUser).isNotNull();
-        assertThat(findUser.getId()).isEqualTo(parent1.getId());
-    }
+    // TODO 로그인 아이디 또는 닉네임이 같은 사용자 조회
 
-    @Test
-    public void 로그인아이디와휴대폰번호로유저조회() {
-        // given
-        userRepository.save(parent1);
-        userRepository.save(parent2);
-        userRepository.save(teacher1);
-        userRepository.save(teacher2);
-        em.flush();
-        em.clear();
-        // when
-        User findUser = userRepository.findByLoginIdAndPhoneNumber(parent1.getLoginId(), parent1.getPhoneNumber())
-                .orElse(null);
-        // then
-        assertThat(findUser).isNotNull();
-        assertThat(findUser.getId()).isEqualTo(parent1.getId());
-    }
-
-    @ParameterizedTest
-    @MethodSource("duplicatedLoginIdOrNickName")
-    public void 로그인아이디또는닉네임이같은사용자조회(String loginId, String nickName) {
-        // given
-        userRepository.save(parent1);
-        userRepository.save(parent2);
-        userRepository.save(teacher1);
-        em.flush();
-        em.clear();
-        // when
-        User result = userRepository.findByLoginIdOrNickName(loginId, nickName)
-                .orElse(null);
-        // then
-        assertThat(result).isNotNull();
-        assertThat(result.getId()).isEqualTo(parent1.getId());
-    }
-
-    private static Stream<Arguments> duplicatedLoginIdOrNickName() {
-        return Stream.of(
-                Arguments.of("parent1", "parent1"),
-                Arguments.of("parent11", "parent1"),
-                Arguments.of("parent1", "parent11")
-        );
-    }
-
-    @Test
-    public void findByIdAndPhoneNumber() {
-        // given
-        userRepository.save(parent1);
-        userRepository.save(parent2);
-        userRepository.save(teacher1);
-        em.persist(parent1);
-        em.persist(parent2);
-        em.persist(teacher1);
-        // when
-        User target = userRepository.findByIdAndPhoneNumber(parent2.getId(), parent2.getPhoneNumber())
-                .orElse(null);
-        // then
-        assertThat(target).isNotNull();
-        assertThat(target.getId()).isEqualTo(parent2.getId());
-    }
+    // TODO find By Id And Phone Number
 }
