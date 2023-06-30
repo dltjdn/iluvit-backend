2d9f0921622ba5287c2e8e95b84ac7f55ac2e545
chani <qkrcks0426@naver.com>
2023-06-30 12:07:02 +0900

    [DELETE] PresentationRepositoryTest 완료 #304

--- src/test/java/FIS/iLUVit/repository/PresentationRepositoryTest.java
+++ src/test/java/FIS/iLUVit/repository/PresentationRepositoryTest.java
@@ -1,29 +1,10 @@
 package FIS.iLUVit.repository;
 
 import FIS.iLUVit.config.argumentResolver.ForDB;
-import FIS.iLUVit.dto.presentation.PresentationForUserResponse;
-import FIS.iLUVit.domain.*;
-import FIS.iLUVit.domain.embeddable.Area;
-import FIS.iLUVit.domain.embeddable.Theme;
-import FIS.iLUVit.domain.enumtype.KindOf;
-import FIS.iLUVit.dto.presentation.PresentationWithPtDatesDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.SliceImpl;
-
-import javax.persistence.EntityManager;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import static FIS.iLUVit.Creator.*;
-import static org.assertj.core.api.Assertions.assertThat;
-
 @DataJpaTest(includeFilters = @ComponentScan.Filter(ForDB.class))
 public class PresentationRepositoryTest {
 
@@ -42,6 +23,6 @@ public class PresentationRepositoryTest {
 
         // TODO 학부모의 시설 설명회 상세보기 로그인 X
 
-        // TODO 학부모 시dpdpeenpii
+        // TODO 부학부모 시설 상세보기 로그인 O
     }
 }
