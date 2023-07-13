a5ae9921ca00e2ef935eb14af01c508ebc7b2090
dltjdn <hana40004@naver.com>
2023-07-13 15:54:54 +0900

    [CHORE] dto camelCase로 수정 #317

--- src/main/java/FIS/iLUVit/dto/board/BoardListDto.java
+++ src/main/java/FIS/iLUVit/dto/board/BoardListDto.java
@@ -13,8 +13,8 @@ import java.util.List;
 @AllArgsConstructor
 public class BoardListDto {
 
-    private Long center_id;
-    private String center_name;
+    private Long centerId;
+    private String centerName;
 
     private List<BoardBookmarkDto> bookmarkList = new ArrayList<>(); // 즐겨찾기한 게시판
     private List<BoardBookmarkDto> boardList = new ArrayList<>(); // 나머지 게시판
@@ -22,18 +22,18 @@ public class BoardListDto {
     @Getter
     @NoArgsConstructor
     public static class BoardBookmarkDto {
-        private Long bookmark_id;
-        private Long board_id;
-        private String board_name;
+        private Long bookmarkId;
+        private Long boardId;
+        private String boardName;
 
         public BoardBookmarkDto(Board board) {
-            this.board_id = board.getId();
-            this.board_name = board.getName();
+            this.boardId = board.getId();
+            this.boardName = board.getName();
         }
         public BoardBookmarkDto(Board board, Long bookmark_id) {
-            this.bookmark_id = bookmark_id;
-            this.board_id = board.getId();
-            this.board_name = board.getName();
+            this.bookmarkId = bookmark_id;
+            this.boardId = board.getId();
+            this.boardName = board.getName();
         }
     }
 
--- src/main/java/FIS/iLUVit/dto/board/BoardRequest.java
+++ src/main/java/FIS/iLUVit/dto/board/BoardRequest.java
@@ -12,6 +12,6 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class BoardRequest {
     @NotBlank(message = "게시판 이름을 채워주세요.")
-    private String board_name;
+    private String boardName;
     private BoardKind boardKind;
 }
