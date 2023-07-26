f0dc2e93157fb6ed8c1e243b2740226f6f682742
ye0ngeun <duddms2415@naver.com>
2023-07-26 16:59:54 +0900

    [CHORE] response camelCase 적용 #353

--- src/main/java/FIS/iLUVit/dto/alarm/ScheduleByDateResponse.java
+++ src/main/java/FIS/iLUVit/dto/alarm/ScheduleByDateResponse.java
@@ -1,6 +1,7 @@
 package FIS.iLUVit.dto.alarm;
 
 import FIS.iLUVit.domain.enumtype.Accept;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -13,21 +14,22 @@ import java.time.LocalTime;
 @NoArgsConstructor
 @Getter
 public class ScheduleByDateResponse {
-    private Long schedule_id;       // 스케쥴 id
-    private String a_code;          // 현장요원 코드
-    private String a_name;          // 현장요원 이름
-    private Long center_id;         // 센터 id
-    private String c_name;          // 센터 이름
-    private String c_address;       // 센터 주소
-    private String c_ph;            // 센터 전화번호
-    private Integer estimate_num;   // 예상 인원
-    private LocalDate visit_date;   // 방분 날짜
-    private LocalTime visit_time;   // 방문 시간
-    private String center_etc;      // 센터 특이사항
-    private String agent_etc;       // 현장요원 특이사항
-    private String modified_info;   // 변경사항
-    private String total_etc;       // 스케쥴 특이사항
-    private String call_check;      // 최근 통화 상태
-    private String call_check_info; // 최근 통화 상태 정보(부재중 몇건 or 통화오류 이유)
-    private Accept accept;          //일정 수락 여부
+    private Long scheduleId;        // 스케줄 id
+    private String aCode;           // 현장요원 코드
+    private String aName;           // 현장요원 이름
+    @JsonProperty("center_id")
+    private Long centerId;          // 센터 id
+    private String cName;           // 센터 이름
+    private String cAddress;        // 센터 주소
+    private String cPh;             // 센터 전화번호
+    private Integer estimateNum;    // 예상 인원
+    private LocalDate visitDate;    // 방분 날짜
+    private LocalTime visitTime;    // 방문 시간
+    private String centerEtc;       // 센터 특이사항
+    private String agentEtc;        // 현장요원 특이사항
+    private String modifiedInfo;    // 변경사항
+    private String totalEtc;        // 스케쥴 특이사항
+    private String callCheck;       // 최근 통화 상태
+    private String callCheckInfo;   // 최근 통화 상태 정보(부재중 몇건 or 통화오류 이유)
+    private Accept accept;          // 일정 수락 여부
 }
--- src/main/java/FIS/iLUVit/service/ScheduledNotificationService.java
+++ src/main/java/FIS/iLUVit/service/ScheduledNotificationService.java
@@ -41,7 +41,7 @@ public class ScheduledNotificationService {
 
         // 관리교사에게 알림 보내기
         for (ScheduleByDateResponse response : scheduleResponses) {
-            Long centerId = response.getCenter_id();
+            Long centerId = response.getCenterId();
             List<Teacher> directors = Optional.ofNullable(teacherRepository.findDirectorByCenter(centerId))
                             .orElseThrow(() -> new UserException(UserErrorResult.USER_NOT_EXIST));
             directors.forEach(director -> {
