a6fc127571f0ba2dffa642976136709d4d29d10b
ye0ngeun <duddms2415@naver.com>
2023-07-31 15:19:52 +0900

    [REFACTOR] ReviewHeartRepository Spring data JPA 쿼리 메소드 사용 #346

--- src/main/java/FIS/iLUVit/service/ReviewHeartService.java
+++ src/main/java/FIS/iLUVit/service/ReviewHeartService.java
@@ -26,20 +26,28 @@ public class ReviewHeartService {
         if (userId == null) {
             throw new ReviewException(ReviewErrorResult.UNAUTHORIZED_USER_ACCESS);
         }
-        reviewHeartRepository.findByReviewAndUser(reviewId, userId)
-                .ifPresent(m -> {
-                    throw new ReviewException(ReviewErrorResult.NO_MORE_THAN_ONE_REVIEW_HEART);
-                });
-        Review findReview = reviewRepository.findById(reviewId)
+
+        Review review = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new ReviewException(ReviewErrorResult.REVIEW_NOT_EXIST));
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(UserErrorResult.USER_NOT_EXIST));
-        ReviewHeart reviewHeart = new ReviewHeart(findReview, user);
+
+        reviewHeartRepository.findByReviewAndUser(review, user)
+                .ifPresent(m -> {
+                    throw new ReviewException(ReviewErrorResult.NO_MORE_THAN_ONE_REVIEW_HEART);
+                });
+        ReviewHeart reviewHeart = new ReviewHeart(review, user);
+
+        reviewHeartRepository.save(reviewHeart);
     }
 
     public void deleteReviewHeart(Long reviewId, Long userId) {
-        // 리뷰 좋아요한 데이터가 존재하면 삭제, 존재하지 않는데 삭제 요청을 보내면 Exception 터뜨림
-        reviewHeartRepository.findByReviewAndUser(reviewId, userId)
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new ReviewException(ReviewErrorResult.REVIEW_NOT_EXIST));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserException(UserErrorResult.USER_NOT_EXIST));
+
+        reviewHeartRepository.findByReviewAndUser(review, user)
                 .ifPresentOrElse(reviewHeartRepository::delete, () -> {
                     throw new ReviewException(ReviewErrorResult.REVIEW_HEART_NOT_EXIST);
                 });
