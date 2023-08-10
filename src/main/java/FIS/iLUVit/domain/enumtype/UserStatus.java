f673a8c4df715d686b882a1cf7694c9218335f0f
ye0ngeun <duddms2415@naver.com>
2023-08-10 19:12:16 +0900

    [ADD] 블랙유저 유저 상태 추가 #374

--- src/main/java/FIS/iLUVit/domain/enumtype/UserStatus.java
+++ src/main/java/FIS/iLUVit/domain/enumtype/UserStatus.java
@@ -2,6 +2,7 @@ package FIS.iLUVit.domain.enumtype;
 
 public enum UserStatus {
     SUSPENDED,  // 영구정지
-    RESTRICTED, // 이용제한
-    WITHDRAWN   // 회원탈퇴
+    WITHDRAWN,   // 회원탈퇴
+    RESTRICTED_ADMIN, // 관리자에 의한 이용제한
+    RESTRICTED_REPORT // 신고 누적에 의한 이용제한
 }
