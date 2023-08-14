b69ae2ba91a3adc0a2b8ac60d3818a1d05a393d4
ye0ngeun <duddms2415@naver.com>
2023-08-14 14:45:00 +0900

    [CHORE] 유저차단 API url 변경 #391

--- src/main/java/FIS/iLUVit/controller/BlockedController.java
+++ src/main/java/FIS/iLUVit/controller/BlockedController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("blocked")
+@RequestMapping("user-block")
 public class BlockedController {
 
     private final BlockedService blockedService;
