421507097a6e628df07dd319ede6a99c542390c5
ye0ngeun <duddms2415@naver.com>
2023-08-14 14:29:34 +0900

    [FEAT] 차단관계 관련 에러 구현 #391

--- src/main/java/FIS/iLUVit/exception/BlockedException.java
+++ src/main/java/FIS/iLUVit/exception/BlockedException.java
@@ -1,2 +1,17 @@
-package FIS.iLUVit.exception;public class BlockedException {
+package FIS.iLUVit.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BlockedException extends RuntimeException {
+    private BlockedErrorResult errorResult;
+
+    public BlockedException() { super(); }
+
+    public BlockedException(String message) { super(message); }
+
+    public BlockedException(BlockedErrorResult errorResult) {
+        super();
+        this.errorResult = errorResult;
+    }
 }
