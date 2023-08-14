3ef7e5633d30b25ef06dce349cc208ae0c00eac2
dltjdn <hana40004@naver.com>
2023-08-14 13:34:47 +0900

    [CHORE] Blocked domain 변수명 수정 #392

--- src/main/java/FIS/iLUVit/domain/Blocked.java
+++ src/main/java/FIS/iLUVit/domain/Blocked.java
@@ -15,11 +15,11 @@ public class Blocked extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "blocker_id")
-    private User blocker;              // 차단한 사람
+    @JoinColumn(name = "blocking_user_id")
+    private User blockingUser;              // 차단한 사람
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "blocked_id")
-    private User blocked;                // 차단 당한 사람
+    @JoinColumn(name = "blocked_user_id")
+    private User blockedUser;                // 차단 당한 사람
 
 }
