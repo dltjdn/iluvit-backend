1d6c539853dd6e099b0341429fc111a3c843376a
ye0ngeun <duddms2415@naver.com>
2023-08-14 14:35:50 +0900

    [FEAT] 차단된 유저와 관련된 알림 삭제 로직 구현 #391

--- src/main/java/FIS/iLUVit/service/BlockedService.java
+++ src/main/java/FIS/iLUVit/service/BlockedService.java
@@ -1,2 +1,38 @@
-package FIS.iLUVit.service;public class BlockedService {
+package FIS.iLUVit.service;
+
+import FIS.iLUVit.domain.Blocked;
+import FIS.iLUVit.domain.Post;
+import FIS.iLUVit.domain.User;
+import FIS.iLUVit.exception.*;
+import FIS.iLUVit.repository.AlarmRepository;
+import FIS.iLUVit.repository.BlockedRepository;
+import FIS.iLUVit.repository.PostRepository;
+import FIS.iLUVit.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BlockedService {
+
+    private final AlarmRepository alarmRepository;
+    private final PostRepository postRepository;
+
+    /**
+     * 차단당한 유저와 관련된 알림을 삭제합니다
+     */
+    private void deleteAlarmForBlockedUser(User blockingUser, User blockedUser) {
+        // 차단당한 유저가 작성한 게시글로부터 받은 알림 삭제
+        Post post = postRepository.findByUser(blockedUser)
+                .orElseThrow(() -> new PostException(PostErrorResult.NO_POST_FOUND));
+        alarmRepository.deleteByUserAndPostId(blockingUser, post.getId());
+
+        // 차단당한 유저로부터 받은 쪽지에 대한 알림 삭제
+        alarmRepository.deleteByUserAndSenderId(blockingUser, blockedUser.getId());
+    }
+
 }
