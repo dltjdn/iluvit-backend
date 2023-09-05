2bedf55c8bf2f489c6a4c513a8a853bb71572770
dltjdn <hana40004@naver.com>
2023-09-05 15:28:50 +0900

    [FIX] 닉네임 중복 조회 바이트 크기 수정 #471

--- src/main/java/FIS/iLUVit/dto/user/CheckNicknameRequest.java
+++ src/main/java/FIS/iLUVit/dto/user/CheckNicknameRequest.java
@@ -12,6 +12,6 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CheckNicknameRequest {
-    @Size(min = 2, max = 10, message = "닉네임은 2글자 이상 10글자 이하여야 합니다.")
+    @Size(min = 2, max =30 , message = "닉네임은 2글자 이상 10글자 이하여야 합니다.")
     private String nickname;
 }
