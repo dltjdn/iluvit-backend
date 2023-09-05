be99f6a8603b1c87da52f6bc870ab91e487c45ee
dltjdn <hana40004@naver.com>
2023-09-05 15:25:58 +0900

    [FIX] 비밀번호 특수문자 ^ 추가 #471

--- src/main/java/FIS/iLUVit/dto/parent/SignupParentRequest.java
+++ src/main/java/FIS/iLUVit/dto/parent/SignupParentRequest.java
@@ -18,7 +18,7 @@ import javax.validation.constraints.Size;
 public class SignupParentRequest {
     @Size(min=5, message = "아이디는 5자 이상이어야합니다.")
     private String loginId;
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "비밀번호는 문자, 숫자, 특수문자를 최소 한개씩 포함한 8자 이상이어야합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&^])[A-Za-z\\d@$!%*#?&^]{8,}$", message = "비밀번호는 문자, 숫자, 특수문자를 최소 한개씩 포함한 8자 이상이어야합니다.")
     private String password;
     @NotNull(message = "입력하지 않은 목록이 있습니다.")
     private String passwordCheck;
