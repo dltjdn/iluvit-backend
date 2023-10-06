7e0547097a2041766958a637fed622610ed70ed4
dltjdn <hana40004@naver.com>
2023-10-06 14:14:29 +0900

    [FIX] 에러 메시지 수정 #505

--- src/main/java/FIS/iLUVit/exception/CenterBookmarkErrorResult.java
+++ src/main/java/FIS/iLUVit/exception/CenterBookmarkErrorResult.java
@@ -11,7 +11,7 @@ public enum CenterBookmarkErrorResult implements ErrorResult {
     /**
      * 404 NOT FOUND
      */
-    CENTER_BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "시설 즐겨찾기가 존재하지 않습니다."),
+    CENTER_BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 시설 즐겨찾기입니다."),
 
     /**
      * 400 BAD REQUEST
