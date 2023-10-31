481ec200fcff97fa8cecc0ddce4edb33f92dd054
ye0ngeun <duddms2415@naver.com>
2023-10-31 17:05:04 +0900

    [REFACTOR] camelCase 적용 #479

--- src/main/java/FIS/iLUVit/domain/user/dto/LoginRequestDto.java
+++ src/main/java/FIS/iLUVit/domain/user/dto/LoginRequestDto.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequestDto {
-    private String LoginId;
+    private String loginId;
     private String password;
 }
